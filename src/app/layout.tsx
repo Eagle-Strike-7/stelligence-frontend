bf9aa2fa151c7c25f6a4f3b70b3fd7f69df9f124
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-15 22:57:06 +0900

    [#3] fix: html 설정 언어 en->ko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,19 +8,19 @@ import '../styles/globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 // NOTE : 화살표 함수 형식으로 변경된 RootLayout 컴포넌트
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+function RootLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
 
     const isLoginPage = pathname === '/login';
 
     return (
-        <html lang="en">
+        <html lang="ko">
             <body className={inter.className}>
                 {!isLoginPage && <Header />}
                 {children}
             </body>
         </html>
     );
-};
+}
 
 export default RootLayout;
