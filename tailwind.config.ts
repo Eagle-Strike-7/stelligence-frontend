e7a1ea466639bb78740123c5ad6e3154dc8196f1
moonjw0 <jjwon1230@naver.com>
2024-01-16 16:59:52 +0900

    [#21] setting: tailwind 색상 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,57 @@ const config: Config = {
                 'gradient-conic':
                     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
             },
+            screens: {
+                mobile: '0rem',
+                desktop: '62rem',
+            },
+            colors: {
+                background: {
+                    light: '#ffffff', 
+                },
+                accent: {
+                    light: '#242372', 
+                },
+                header: {
+                    light: '#07063B', 
+                },
+                text: {
+                    light: '#000000',
+                    dark: '#ffffff',
+                },
+                // TODO: light, dark에서 공통으로 쓸 색상으로 변경
+                vote: {
+                    start: '#38A169',
+                    approve: '#FF0000',
+                    reject: '#4236D4',
+                },
+                section: {
+                    addBg: '#CDF4CA',
+                    deleteBg: '#FF0000',
+                },
+
+                blue: {
+                    50: '#EBF8FF',
+                    100: '#BEE3F8',
+                    200: '#90CDF4',
+                    300: '#63B3ED',
+                    400: '#4299E1',
+                    500: '#3182CE',
+                    600: '#2B6CB0',
+                    700: '#2C5282',
+                    800: '#2A4365',
+                    900: '#1A365D',
+                },
+                gray: {
+                    50: '#EDF2F7', // requestButton
+                    100: '#FDFDFD', // sectionBg
+                    200: '#E2E8F0', // containerBorder
+                    400: '#A0AEC0', // placeholder
+                }
+            },
         },
     },
     plugins: [],
 };
 export default config;
+
