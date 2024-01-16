df87b7f7c5ffa23c4239c7069e9ce0f25d57f742
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-16 12:18:33 +0900

    [#15] style: 메인 페이지 검색 창 UI 구현

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,35 @@
 import Wrapper from '@/components/Common/Wrapper';
+import {
+    Button,
+    Center,
+    Input,
+    InputGroup,
+    InputRightElement,
+} from '@chakra-ui/react';
+import { BiSearch } from 'react-icons/bi';
 
 const Home = () => {
     return (
         <Wrapper>
-            <div>Home</div>
+            <Center>
+                <InputGroup mt="2rem">
+                    <Input
+                        w="26.5rem"
+                        h="3rem"
+                        p="1rem"
+                        pr="3.5rem"
+                        fontSize="1rem"
+                        placeholder="어떤 별을 찾으시나요?"
+                        fill="gray"
+                        className="bg-gray-100 rounded-md"
+                    />
+                    <InputRightElement>
+                        <Button h="3rem" size="lg" pr="1rem">
+                            <BiSearch size="1.5rem" className="h-full" />
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </Center>
         </Wrapper>
     );
 };
