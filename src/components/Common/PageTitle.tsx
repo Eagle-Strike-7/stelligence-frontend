276e5712c2e74c99bc7bb6d55e4d93e85f9a9cab
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-16 20:29:15 +0900

    [#28] style: 공통 컴포넌트-PageTitle 컴포넌트 생성

---
+++ src/components/Common/PageTitle.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+
+const PageTitle = () => {
+    return (
+        <Heading fontSize="1.5rem" fontWeight={600} className="mb-4">
+            마리모
+        </Heading>
+    );
+};
+
+export default PageTitle;
