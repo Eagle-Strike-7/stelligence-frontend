60bad67f4e29ba1ffe635fce7812e672c96a30b2
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-16 16:11:20 +0900

    [#11] style: 전역 폰트 설정 및 next optimize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                variable: ['Pretendard', 'inter', 'sans-serif'],
+            },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
