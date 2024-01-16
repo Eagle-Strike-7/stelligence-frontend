c8835ff8de4f33cca4a1a0aa1f50a1561c864cb6
LeeDahee <dduosy9@gmail.com>
2024-01-16 19:33:48 +0900

    [#25] setting: theme에 border radius 추가

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -76,6 +76,17 @@ const customTheme = extendTheme({
         extrabold: 800,
         black: 900,
     },
+    radii: {
+        none: '0',
+        sm: '0.125rem',
+        base: '0.25rem',
+        md: '0.375rem',
+        lg: '0.5rem',
+        xl: '0.75rem',
+        '2xl': '1rem',
+        '3xl': '1.5rem',
+        full: '9999px',
+    },
 });
 
 export default customTheme;
