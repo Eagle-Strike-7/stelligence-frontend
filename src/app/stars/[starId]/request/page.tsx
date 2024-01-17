99991861780ec6b41f14e0f52a936c8b20472e79
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-17 16:19:56 +0900

    [#32] style: 특정 글 수정 요청(요청중) 페이지 생성 및 UI 구현

---
+++ src/app/stars/[starId]/request/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React from 'react';
+import { useParams } from 'next/navigation';
+import Wrapper from '@/components/Common/Wrapper';
+import PageTitle from '@/components/Common/PageTitle';
+import StateTab from '@/components/Common/StateTab';
+import dummyDebateList from '@/constants/dummyData';
+import ListCard from '@/components/Common/ListCard';
+import Pagination from '@mui/material/Pagination';
+import { Center } from '@chakra-ui/react';
+
+const Page: React.FC = () => {
+    const params = useParams<{ starId: string }>();
+    const id = params.starId;
+
+    return (
+        <Wrapper>
+            <PageTitle pageTitle="마리모" />
+            <StateTab tab1="요청중" tab2="완료" />
+
+            {dummyDebateList.map(item => {
+                return (
+                    <ListCard
+                        key={id}
+                        title={item.title}
+                        username={item.username}
+                        time={item.time}
+                        content={item.content}
+                        likeNum={item.likeNum}
+                        dislikeNum={item.dislikeNum}
+                    />
+                );
+            })}
+            <Center>
+                <Pagination
+                    count={10}
+                    showFirstButton
+                    showLastButton
+                    className="my-10 mb-20"
+                />
+            </Center>
+        </Wrapper>
+    );
+};
+
+export default Page;
