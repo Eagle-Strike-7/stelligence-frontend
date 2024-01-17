f36900fa16a07d69c17b3aaa435a5d93e22a7aea
LeeDahee <dduosy9@gmail.com>
2024-01-17 21:37:12 +0900

    [#35] setting: chakraProvider 적용, rootlayout 화살표함수

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,12 @@ import { usePathname } from 'next/navigation';
 import Header from '@/components/Header/Header';
 import { Inter } from 'next/font/google';
 import '../styles/globals.css';
+import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
 // NOTE : 화살표 함수 형식으로 변경된 RootLayout 컴포넌트
-function RootLayout({ children }: { children: React.ReactNode }) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
     const pathname = usePathname();
 
     const isLoginPage = pathname === '/login';
@@ -17,10 +18,10 @@ function RootLayout({ children }: { children: React.ReactNode }) {
         <html lang="ko">
             <body className={inter.className}>
                 {!isLoginPage && <Header />}
-                {children}
+                <Providers>{children}</Providers>
             </body>
         </html>
     );
-}
+};
 
 export default RootLayout;
