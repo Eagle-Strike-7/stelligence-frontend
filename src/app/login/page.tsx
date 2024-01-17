207902f1ca07f7dc1da5034f8945d62916abfb2b
LeeDahee <dduosy9@gmail.com>
2024-01-17 20:33:20 +0900

    [#6] feat: 로그인 페이지 생성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,21 @@
+import GoogleLogin from '@/components/Login/GoogleLogin';
+import KakaoLogin from '@/components/Login/KakaoLogin';
+import NaverLogin from '@/components/Login/NaverLogin';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div className="">
+            <Box>
+                <div className="">
+                    <KakaoLogin />
+                    <NaverLogin />
+                    <GoogleLogin />
+                </div>
+            </Box>
+        </div>
+    );
 };
 
 export default page;
