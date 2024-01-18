880f40d7efd72fc463a2216ea2f4d77825dd3333
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-19 00:55:17 +0900

    [#47] style: 로그인 아이콘 적용

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -1,26 +1,29 @@
 import { Tooltip } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
+import { AiOutlineLogin } from 'react-icons/ai';
 
 const Login = () => {
-    return (
-        <div className="mr-20">
-            <Tooltip
-                hasArrow
-                defaultIsOpen
-                arrowSize={10}
-                label="우주로 출발하기🚀"
-                placement="left"
-                color="black"
-                backgroundColor="#f6f6f6"
-                size="lg"
-                padding="0.25rem 0.75rem"
-                className="rounded-md"
-            >
-                <Link href="/login">로그인</Link>
-            </Tooltip>
-        </div>
-    );
+  return (
+    <div className="mr-20  w-12">
+      <Tooltip
+        hasArrow
+        defaultIsOpen
+        arrowSize={10}
+        label="우주로 출발하기🚀"
+        placement="left"
+        color="black"
+        backgroundColor="#f6f6f6"
+        size="lg"
+        padding="0.25rem 0.75rem"
+        className="rounded-md"
+      >
+        <Link href="/login">
+          <AiOutlineLogin className="w-6 h-6" />
+        </Link>
+      </Tooltip>
+    </div>
+  );
 };
 
 export default Login;
