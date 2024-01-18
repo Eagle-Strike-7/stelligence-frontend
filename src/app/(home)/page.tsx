d8df23fc3c6e5adc4eb7e097393cc596c35aef1f
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-18 12:07:21 +0900

    [#41] fix: 메인페이지 수정(가운데 정렬 및 chakra ui 설정 변경)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,20 +12,31 @@ const Home = () => {
     return (
         <Wrapper>
             <Center>
-                <InputGroup>
+                <InputGroup
+                    width="26.5rem"
+                    height="3rem"
+                    className="flex justify-center place-items-center"
+                >
                     <Input
-                        w="26.5rem"
-                        h="3rem"
-                        p="1rem"
-                        pr="3.5rem"
-                        fontSize="1rem"
+                        variant="outline"
+                        pr="4rem"
+                        size="lg"
                         placeholder="어떤 별을 찾으시나요?"
-                        fill="gray"
-                        className="bg-gray-100 rounded-md"
+                        className="rounded-md"
                     />
-                    <InputRightElement>
-                        <Button h="3rem" size="lg" pr="1rem">
-                            <BiSearch size="1.5rem" className="h-full" />
+                    <InputRightElement
+                        width="4rem"
+                        height="3rem"
+                        className="flex my-auto mr-1"
+                    >
+                        <Button
+                            w="full"
+                            size="md"
+                            variant="ghost"
+                            colorScheme="facebook"
+                            className="mr-1 h-10 w-10"
+                        >
+                            <BiSearch className="w-6 h-6" />
                         </Button>
                     </InputRightElement>
                 </InputGroup>
