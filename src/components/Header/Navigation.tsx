28896cedf8e519371b410479d1dad3af7bddd188
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-19 00:56:07 +0900

    [#47] refactor: navigation 코드 규칙에 맞게 리팩토링 및 디자인 수정

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -1,29 +1,45 @@
-import { Heading } from '@chakra-ui/react';
+import { Button, Heading } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
+import { AiOutlineComment, AiOutlineLike } from 'react-icons/ai';
 
 const Navigation = () => {
-    return (
-        <div className="ml-20">
-            <ul>
-                <li className="inline mr-2">
-                    <Link href="/voteList">
-                        <Heading size="md" className="inline">
-                            투표
-                        </Heading>
-                    </Link>
-                </li>
-
-                <li className="inline">
-                    <Link href="/debateList">
-                        <Heading size="md" className="inline">
-                            토론
-                        </Heading>
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="flex ml-20 w-12">
+      <div className="inline mr-2">
+        <Link href="/voteList">
+          <Button
+            leftIcon={<AiOutlineLike size="20px" />}
+            variant="ghost"
+            textColor="white"
+            _hover={{ bg: '#ebedf0', textColor: 'black', fontWeight: 600 }}
+            cursor="pointer"
+            size="sm"
+          >
+            <Heading size="sm" fontWeight={600}>
+              투표
+            </Heading>
+          </Button>
+        </Link>
+      </div>
+      <div className="inline">
+        <Link href="/debateList">
+          <Button
+            leftIcon={<AiOutlineComment size="20px" />}
+            variant="ghost"
+            textColor="white"
+            _hover={{ bg: '#ebedf0', textColor: 'black', fontWeight: 600 }}
+            cursor="pointer"
+            size="sm"
+          >
+            <Heading size="sm" fontWeight={600}>
+              토론
+            </Heading>
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
