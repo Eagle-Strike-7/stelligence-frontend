357853e1fb20b0f8616a68b87d7e5a95b0bfa8dc
moonjw0 <jjwon1230@naver.com>
2024-01-18 12:54:02 +0900

    [#4] fix: 인터페이스 타입 이름 수정

--- src/components/Common/StarSection.tsx
+++ src/components/Common/StarSection.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 // FIXME: 타입은 기능구현 때 변경 예정
 export interface StarSectionProps {
   id: number,
-  section_title: string,
-  section_content: string
+  sectionTitle: string,
+  sectionContent: string
 }
 
 // NOTE: 글 상세보기 섹션 컴포넌트 
-const StarSection = ({section_content}: Pick<StarSectionProps, 'section_content'> ) => {
+const StarSection = ({sectionContent}: Pick<StarSectionProps, 'sectionContent'> ) => {
   return (
     <div className="border-2 rounded-lg p-4 bg-section-Bg">
-      {section_content}
+      {sectionContent}
     </div>
   )
 }
