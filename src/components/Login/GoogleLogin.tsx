f971521944bfa5ca74c4b75e9f46fd37fbf2632e
LeeDahee <dduosy9@gmail.com>
2024-01-21 00:42:02 +0900

    [#38] feat: 구글 로그인 client id, redirect uri 링크 연결

--- src/components/Login/GoogleLogin.tsx
+++ src/components/Login/GoogleLogin.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
 
 const GoogleLogin = () => {
+    const link = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email profile`;
+    const handleClick = () => {
+        window.location.href = link;
+    };
     return (
         <button
             type="button"
             className="flex item-center p-3 w-60 h-15 rounded-xl border border-gray-400 bg-white"
+            onClick={handleClick}
         >
             <Image
                 src="/image/googleLogo.png"
