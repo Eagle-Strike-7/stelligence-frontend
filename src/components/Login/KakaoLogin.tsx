c07c404545a7b3552157c30eacc018e332f03c32
LeeDahee <dduosy9@gmail.com>
2024-01-20 16:41:28 +0900

    [#39] feat: 카카오 rest api key, redirect uri 링크 연결

--- src/components/Login/KakaoLogin.tsx
+++ src/components/Login/KakaoLogin.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 
 const KakaoLogin = () => {
+    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    const handleClick = () => {
+        window.location.href = link;
+    };
     return (
-        <button type="button" className="w-60 h-15">
+        <button type="button" className="w-60 h-15" onClick={handleClick}>
             <Image
                 src="/image/kakaoLogin.png"
                 alt="카카오 로그인 버튼"
