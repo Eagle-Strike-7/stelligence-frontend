f5ae0748285fcb0d8023e097340619fc91870897
LeeDahee <dduosy9@gmail.com>
2024-01-21 01:27:41 +0900

    [#6] refactor: 팀 규칙 반영 - Box를 Card로 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,32 +2,32 @@ import Wrapper from '@/components/Common/Wrapper';
 import GoogleLogin from '@/components/Login/GoogleLogin';
 import KakaoLogin from '@/components/Login/KakaoLogin';
 import NaverLogin from '@/components/Login/NaverLogin';
-import { Box } from '@chakra-ui/react';
+import { Card } from '@chakra-ui/react';
 import { DM_Serif_Display } from 'next/font/google';
 import React from 'react';
 
 const dm = DM_Serif_Display({ subsets: ['latin'], weight: ['400'] });
 
 const page = () => {
-    return (
-        <Wrapper>
-            <div className="flex flex-col gap-y-8 justify-center items-center h-screen">
-                <h1 className={`${dm.className} text-4xl`}>Stelligence</h1>
-                <Box
-                    borderWidth="1px"
-                    borderColor="requestBtn"
-                    p="20"
-                    borderRadius="lg"
-                >
-                    <div className="flex flex-col gap-y-3">
-                        <KakaoLogin />
-                        <NaverLogin />
-                        <GoogleLogin />
-                    </div>
-                </Box>
-            </div>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <div className="flex flex-col gap-y-8 justify-center items-center h-screen">
+        <h1 className={`${dm.className} text-4xl`}>Stelligence</h1>
+        <Card
+          borderWidth="1px"
+          borderColor="requestBtn"
+          p="20"
+          borderRadius="lg"
+        >
+          <div className="flex flex-col gap-y-3">
+            <KakaoLogin />
+            <NaverLogin />
+            <GoogleLogin />
+          </div>
+        </Card>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default page;
