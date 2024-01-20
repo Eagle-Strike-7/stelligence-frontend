7e68a456b5a2837e41f551f8da632beba1d33667
LeeDahee <dduosy9@gmail.com>
2024-01-20 16:57:40 +0900

    [#39] feat: 네이버 로그인 client id, redirect uri 링크 연결

--- src/components/Login/NaverLogin.tsx
+++ src/components/Login/NaverLogin.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
 
 const NaverLogin = () => {
+    const link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}`;
+    const handleClick = () => {
+        window.location.href = link;
+    };
     return (
         <button
             type="button"
             className="flex w-60 h-15 rounded-lg bg-naver-light"
+            onClick={handleClick}
         >
             <Image
                 src="/image/naverLogo.png"
