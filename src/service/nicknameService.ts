3bd508e6e1e1183bfdfdd1d6af727939a6a16838
LeeDahee <dduosy9@gmail.com>
2024-01-22 02:48:17 +0900

    [#55] feat: 닉네임 조회 함수

--- src/service/nicknameService.ts
+++ src/service/nicknameService.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
+// NOTE 닉네임 조회
+// TODO 백엔드 실제 url로 변경, response 받은 후 작업
+export const getNickname = () => {
+  axios
+    .get('api/members/me/nickname')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
 const putNickname = (nickname: string) => {
   axios({
     method: 'PUT',
