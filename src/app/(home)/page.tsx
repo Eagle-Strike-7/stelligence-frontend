3190fff8407e2d2d602e160233da89fb29e0a848
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-21 15:50:37 +0900

    [#53] chore: 그래프 더미데이터 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,56 +5,63 @@ import Wrapper from '@/components/Common/Wrapper';
 import GalaxyGraph from './GalaxyGraph';
 
 const Home = () => {
-    const nodes = Array.from({ length: 50 }, (_, i) => {
-        return {
-            id: `${i + 1}`,
-            group: (Math.floor(i / 10) + 1).toString(),
-        };
-    });
+  const nodes = [{ id: 'root', group: '0' }];
+  const links = [];
 
-    const links = [];
-    for (let i = 1; i < nodes.length; i += 1) {
-        links.push({ source: '1', target: `${i + 1}` }); // 중앙 노드와 다른 노드들을 연결
+  const addChildNodes = (parentId, groupId, numChildren) => {
+    for (let i = 0; i < numChildren && nodes.length < 100; i += 1) {
+      const nodeId = `node${groupId}_${i}`;
+      nodes.push({ id: nodeId, group: groupId }); // 그룹 ID를 groupId로 설정
+      links.push({ source: parentId, target: nodeId });
     }
+  };
 
-    // 추가적인 연결을 생성하여 원 형태에 가깝게 구성
-    for (let i = 1; i < nodes.length - 1; i += 1) {
-        if (i % 10 === 0) {
-            links.push({ source: `${i}`, target: `${i + 2}` });
-        } else {
-            links.push({ source: `${i}`, target: `${i + 1}` });
-        }
-    }
+  // 각 그룹의 루트 노드 추가 및 자식 노드 생성
+  let groupId = 1;
+  while (nodes.length < 100) {
+    // 새로운 그룹의 루트 노드 생성
+    const rootId = `root${groupId}`;
+    nodes.push({ id: rootId, group: `${groupId}` });
 
-    return (
-        <Wrapper>
-            <div className="flex flex-col justify-center place-items-center">
-                <div className="flex flex-col w-100">
-                    <InputGroup>
-                        <Input
-                            w="26.5rem"
-                            h="3rem"
-                            p="1rem"
-                            pr="3.5rem"
-                            fontSize="1rem"
-                            placeholder="어떤 별을 찾으시나요?"
-                            fill="gray"
-                            className="bg-gray-100 rounded-md"
-                        />
-                        <InputRightElement>
-                            <Button h="3rem" size="lg" pr="1rem">
-                                <BiSearch size="1.5rem" className="h-full" />
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                </div>
+    // 자식 노드 추가
+    const numChildren = Math.min(
+      100 - nodes.length,
+      3 + Math.floor(Math.random() * 4),
+    ); // 3에서 6개의 자식 노드 무작위 추가
+    addChildNodes(rootId, groupId, numChildren);
 
-                <div className="flex ">
-                    <GalaxyGraph nodes={nodes} links={links} />
-                </div>
-            </div>
-        </Wrapper>
-    );
+    groupId += 1;
+  }
+
+  return (
+    <Wrapper>
+      <div className="flex flex-col justify-center place-items-center">
+        <div className="flex flex-col w-100">
+          <InputGroup>
+            <Input
+              w="26.5rem"
+              h="3rem"
+              p="1rem"
+              pr="3.5rem"
+              fontSize="1rem"
+              placeholder="어떤 별을 찾으시나요?"
+              fill="gray"
+              className="bg-gray-100 rounded-md"
+            />
+            <InputRightElement>
+              <Button h="3rem" size="lg" pr="1rem">
+                <BiSearch size="1.5rem" className="h-full" />
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </div>
+
+        <div className="flex ">
+          <GalaxyGraph nodes={nodes} links={links} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Home;
