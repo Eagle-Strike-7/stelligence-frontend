51ded64b5acf93b4220cc3eea76f7c85adf66ecf
LeeDahee <dduosy9@gmail.com>
2024-01-24 01:32:11 +0900

    [#71] feat: 닉네임 수정 기능 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Wrapper from '@/components/Common/Wrapper';
 import TitleCard from '@/components/Common/TitleCard';
 import { Avatar, Badge, Button, Input } from '@chakra-ui/react';
 import Link from 'next/link';
+import axios from 'axios';
 import MyBadge from './components/MyBadge';
 
-// FIXME 백엔드 통신 후 삭제
+// TODO 백엔드 통신 후 삭제
 const dummyUserData = {
   id: 1,
   profile: '',
@@ -31,6 +34,41 @@ const dummyUserData = {
 };
 
 const Page = () => {
+  // TODO 백엔드 데이터 받아온 뒤 initNickname 값 변경
+  const initNickname = dummyUserData.nickname;
+  const [newNickname, setNewNickname] = useState(initNickname);
+  const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewNickname(e.target.value);
+  };
+  const handleSaveNewNickname = async () => {
+    if (initNickname === newNickname) {
+      // TODO alert 대신에 Toast로 변경
+      alert('이미 사용한 닉네임과 같습니다.');
+      return;
+    }
+
+    // TODO 실제 백엔드 url로 변경 후 response 부분 다시 작성
+    try {
+      const response = await axios({
+        method: 'PUT',
+        url: 'http://localhost:8080/api/members/me/nickname',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: {
+          nickname: newNickname,
+        },
+      });
+
+      if (response.data.success === 'fail') {
+        alert(response.data.message);
+      } else {
+        console.log('닉네임 수정 성공');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Wrapper>
       <div className="flex flex-col gap-8">
@@ -49,14 +87,20 @@ const Page = () => {
               <div className="flex">
                 <span className="flex text-sm items-center">닉네임</span>
                 <Input
-                  defaultValue={dummyUserData.nickname}
+                  defaultValue={newNickname}
+                  onChange={handleChangeNickname}
                   placeholder="닉네임을 입력하세요."
                   width="md"
                   ml="2"
                   fontSize="sm"
                   size="sm"
                 />
-                <Button colorScheme="green" size="sm" ml="5">
+                <Button
+                  colorScheme="green"
+                  size="sm"
+                  ml="5"
+                  onClick={handleSaveNewNickname}
+                >
                   변경사항 저장
                 </Button>
               </div>
