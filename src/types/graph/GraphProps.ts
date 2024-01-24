00d6827a3e0210c586b8da37a30b3da54f6c40bc
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-24 23:58:50 +0900

    [#73] feat: 그래프 관련 타입 파일 분리

---
+++ src/types/graph/GraphProps.ts
@@ -0,0 +1,26 @@
+export interface GraphNode extends d3.SimulationNodeDatum {
+  id: string;
+  group: string;
+  title: string;
+  x?: number;
+  y?: number;
+  fx?: number | null;
+  fy?: number | null;
+}
+
+export interface GraphLink extends d3.SimulationLinkDatum<GraphNode> {
+  source: string | GraphNode;
+  target: string | GraphNode;
+}
+
+export interface SearchResult {
+  documentId: string;
+  title: string;
+  group: string;
+}
+
+export interface ServerLink {
+  linkId: number;
+  parentDocumentId: number;
+  childDocumentId: number;
+}
