5a17b56c745d52309813581c01784841d5cf3fd1
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-25 00:03:16 +0900

    [#73] feat: 그래프 뷰 조회, 검색 서버 연결 및 데이터 상태 저장

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,77 +1,71 @@
 'use client';
 
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
 import { BiSearch } from 'react-icons/bi';
-import axios from 'axios';
-import GalaxyGraph, { Link } from './components/GalaxyGraph';
-
-interface SearchResult {
-  documentId: string;
-  title: string;
-  groupt: string;
-}
+import {
+  transformResults,
+  transformLinks,
+} from '@/hooks/graph/transformGraphInfo';
+import { GraphNode, GraphLink, SearchResult } from '@/types/graph/GraphProps';
+import GalaxyGraph from './components/GalaxyGraph';
+import '../../styles/graph.module.css';
 
 const Home = () => {
   const [searchText, setSearchText] = useState<string>('');
   const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [initialNodes, setInitialNodes] = useState<GraphNode[]>([]);
+  const [initialLinks, setInitialLinks] = useState<GraphLink[]>([]);
 
-  const tempUrl = 'http://211.201.26.10:8080/api/documents/search';
+  const SERVER_URL =
+    'http://ec2-43-203-87-227.ap-northeast-2.compute.amazonaws.com';
 
   const handleSearch = () => {
     axios
-      .get(`${tempUrl}?title=${searchText}`)
+      .get(`${SERVER_URL}/api/documents/search`, {
+        params: { title: searchText },
+      })
       .then(response => {
         const { data } = response;
-        const resultIds = data.map((item: SearchResult) => {
-          return item.documentId;
+        const resultIds = data.results.map((item: SearchResult) => {
+          return item.documentId.toString();
         });
         setSearchResults(resultIds);
       })
       .catch(error => {
-        console.error('Error fetching data: ', error);
+        throw new Error('Error fetching data: ', error);
       });
   };
 
-  const { nodes, links } = useMemo(() => {
-    const nodesTemp = [{ id: 'root', group: '0', title: '시작' }];
-    const linksTemp: Link[] = []; // Link 타입 객체들의 배열
+  const fetchGraphData = () =>
+    {return axios
+      .get(`${SERVER_URL}/api/documents`, { params: { depth: 7 } })
+      .then(response => {
+        const { data } = response;
 
-    const addChildNodes = (
-      parentId: string,
-      groupId: string,
-      numChildren: number,
-    ) => {
-      for (let i = 0; i < numChildren && nodesTemp.length < 100; i += 1) {
-        const nodeId = `node${groupId}_${i}`;
-        nodesTemp.push({ id: nodeId, group: groupId, title: nodeId }); // 그룹 ID를 groupId로 설정
-        linksTemp.push({ source: parentId, target: nodeId });
-      }
-    };
+        const transfomedResults = transformResults(data.results.documentNodes);
+        const transformedLinks = transformLinks(data.results.links);
 
-    let groupId = 1;
-    while (nodesTemp.length < 100) {
-      const rootId = `root${groupId}`;
-      nodesTemp.push({ id: rootId, group: `${groupId}`, title: `마리모` });
+        setInitialNodes(transfomedResults);
+        setInitialLinks(transformedLinks);
+      })
+      .catch(error => {
+        throw new Error('Error fetching data: ', error);
+      })};
 
-      const numChildren = Math.min(
-        100 - nodesTemp.length,
-        3 + Math.floor(Math.random() * 4),
-      );
-      addChildNodes(rootId, `${groupId}`, numChildren);
-
-      groupId += 1;
-    }
-    return { nodes: nodesTemp, links: linksTemp };
+  useEffect(() => {
+    fetchGraphData();
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full px-4">
-      <div className="mt-5">
+    <div className="flex flex-col items-center">
+      <div className="mt-8">
         <InputGroup width="full">
           <Input
             w="40rem"
             size="lg"
+            color="white"
             placeholder="어떤 별을 찾으시나요?"
             value={searchText}
             onChange={e => {
@@ -96,8 +90,8 @@ const Home = () => {
 
       <div className="mt-5">
         <GalaxyGraph
-          nodes={nodes}
-          links={links}
+          nodes={initialNodes}
+          links={initialLinks}
           searchResults={searchResults}
         />
       </div>
