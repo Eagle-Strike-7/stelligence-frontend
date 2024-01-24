07ce6b2159e146e480394649c331ecbac8b21174
LeeDahee <dduosy9@gmail.com>
2024-01-25 04:00:30 +0900

    [#84] refactor: 외부 더미데이터로 연결

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -7,31 +7,9 @@ import { Avatar, Badge, Button, Input, useToast } from '@chakra-ui/react';
 import Link from 'next/link';
 import axios from 'axios';
 import MyBadge from './components/MyBadge';
-
-// TODO 백엔드 통신 후 삭제
-const dummyUserData = {
-  id: 1,
-  profile: '',
-  nickname: '독수리타법 7남매',
-  socialType: 'kakao',
-  bookmarks: [
-    { id: 1, title: '깊이 우선 탐색' },
-    { id: 2, title: '알고리즘' },
-    { id: 3, title: '다익스트라 알고리즘' },
-  ],
-  badges: [
-    {
-      id: 1,
-      title: '우주 여행자',
-      image: '/image/astronaut.png',
-    },
-    {
-      id: 2,
-      title: '해왕성 탐사완료',
-      image: '/image/astronaut.png',
-    },
-  ],
-};
+import dummyUserData from '../../constants/dummyUserData.json';
+import dummyBookmarkData from '../../constants/dummyBookmarkData.json';
+import dummyBadgeData from '../../constants/dummyBadgeData.json';
 
 const Page = () => {
   // TODO 백엔드 데이터 받아온 뒤 initNickname 값 변경
@@ -88,7 +66,10 @@ const Page = () => {
       <div className="flex flex-col gap-8">
         <TitleCard title="유저 정보">
           <div className="flex">
-            <Avatar name={dummyUserData.nickname} src={dummyUserData.profile} />
+            <Avatar
+              name={dummyUserData.nickname}
+              src={dummyUserData.profileUrl}
+            />
             <div className="flex flex-col gap-2 ml-4 ">
               <div>
                 <h3 className="inline-block font-bold text-md">
@@ -123,11 +104,11 @@ const Page = () => {
         </TitleCard>
         <TitleCard title="북마크">
           <ul className="flex flex-col gap-1">
-            {dummyUserData.bookmarks.map(bookmark => {
+            {dummyBookmarkData.bookmarks.map(bookmark => {
               return (
-                <li key={bookmark.id}>
+                <li key={bookmark.bookmarkId}>
                   <Link
-                    href={`/stars/${bookmark.id}`}
+                    href={`/stars/${bookmark.documentId}`}
                     className="hover:underline"
                   >
                     {bookmark.title}
@@ -139,12 +120,12 @@ const Page = () => {
         </TitleCard>
         <TitleCard title="배지">
           <div className="flex flex-wrap gap-3">
-            {dummyUserData.badges.map(badge => {
+            {dummyBadgeData.badges.map(badge => {
               return (
                 <MyBadge
-                  title={badge.title}
-                  image={badge.image}
-                  key={badge.id}
+                  title={badge.badgeTitle}
+                  image={`/image/${badge.badgeType}.jpg`}
+                  key={badge.badgeType}
                 />
               );
             })}
