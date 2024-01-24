1220a50445ce1c8c86c919aa7be6e9c07e30dbfb
LeeDahee <dduosy9@gmail.com>
2024-01-24 18:09:07 +0900

    [#80] fix: 소셜로그인 uri 변경

--- src/app/login/components/NaverLogin.tsx
+++ src/app/login/components/NaverLogin.tsx
@@ -5,7 +5,7 @@ import { Button } from '@chakra-ui/react';
 import { SiNaver } from 'react-icons/si';
 
 const NaverLogin = () => {
-  const link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}`;
+  const link = `http://localhost:8080/oauth2/authorization/naver`;
   const handleClick = () => {
     window.location.href = link;
   };
