3846043acf3b0dd8d8de6e3f408dfc13d463556c
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-25 17:19:55 +0900

    [#76] feat: 검색 결과에 따른 드롭다운 바 기본 UI 및 렌더링 구현

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,14 +2,21 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Box,
+  VStack,
+} from '@chakra-ui/react';
 import { BiSearch } from 'react-icons/bi';
 import { useQuery } from '@tanstack/react-query';
 import {
   transformResults,
   transformLinks,
 } from '@/hooks/graph/transformGraphInfo';
-import { Graph, SearchResult } from '@/types/graph/GraphProps';
+import { Graph, GraphNode, SearchResult } from '@/types/graph/GraphProps';
 import GalaxyGraph from './components/GalaxyGraph';
 import '../../styles/graph.module.css';
 
@@ -18,10 +25,9 @@ const SERVER_URL =
 
 const getGraphData = async (): Promise<Graph> => {
   const response = await axios.get(`${SERVER_URL}/api/documents`, {
-    params: { depth: 7 },
+    params: { depth: 3 },
   });
   const { data } = response;
-
   const transformedResults = transformResults(data.results.documentNodes);
   const transformedLinks = transformLinks(data.results.links);
 
@@ -30,7 +36,9 @@ const getGraphData = async (): Promise<Graph> => {
 
 const Home = () => {
   const [searchText, setSearchText] = useState<string>('');
-  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<GraphNode[]>([]);
+  const [serachIds, setSearchIds] = useState<string[]>([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const { data, isError, isLoading } = useQuery<Graph>({
     queryKey: ['graphData'],
@@ -46,7 +54,12 @@ const Home = () => {
         const resultIds = response.data.results.map((item: SearchResult) => {
           return item.documentId.toString();
         });
-        setSearchResults(resultIds);
+        setSearchIds(resultIds);
+        setSearchResults(response.data.results);
+
+        if (response.data.results.length > 0) {
+          setIsDropdownOpen(true);
+        }
       })
       .catch(error => {
         console.error('Error fetching search results: ', error);
@@ -63,16 +76,27 @@ const Home = () => {
   if (isError) return <div>Error loading data</div>;
 
   return (
-    <div className="flex flex-col items-center bg-black pt-2">
-      <div className="mt-8">
-        <InputGroup width="full">
+    <div className="flex flex-col items-center bg-background-dark h-screen pt-2">
+      <div className="mt-8 relative">
+        <InputGroup
+          width="full"
+          zIndex={1}
+          onBlur={() => {return setIsDropdownOpen(false)}}
+        >
           <Input
             w="40rem"
             size="lg"
             color="white"
             placeholder="어떤 별을 찾으시나요?"
-            focusBorderColor="#4e4d9c"
+            focusBorderColor="#121212"
             value={searchText}
+            variant="outline"
+            borderColor="#292929"
+            bg="#292929"
+            _focus={{ borderColor: 'transparent', borderWidth: 2 }}
+            _hover={{ borderColor: 'transparent', borderWidth: 2 }}
+            borderBottomLeftRadius={isDropdownOpen ? 'none' : 'md'}
+            borderBottomRightRadius={isDropdownOpen ? 'none' : 'md'}
             onChange={e => {
               return setSearchText(e.target.value);
             }}
@@ -83,7 +107,7 @@ const Home = () => {
               size="sm"
               paddingX="1rem"
               variant="ghost"
-              _hover={{ bg: '#4e4d9c' }}
+              _hover={{ bg: '#414141' }}
               marginTop="0.5rem"
               onClick={handleSearch}
             >
@@ -91,6 +115,43 @@ const Home = () => {
             </Button>
           </InputRightElement>
         </InputGroup>
+
+        {isDropdownOpen && (
+          <VStack
+            bg="rgba(18,18,18,0.8)"
+            paddingX={2}
+            paddingY={2}
+            spacing={2}
+            borderWidth={2}
+            borderTopWidth={0}
+            align="stretch"
+            position="absolute"
+            color="white"
+            w="40rem"
+            verticalAlign="middle"
+            borderBottomLeftRadius="md"
+            borderBottomRightRadius="md"
+            zIndex="dropdown"
+            borderColor="#292929"
+          >
+            {searchResults.map(result => {
+              return (
+                <Box
+                  key={result.id}
+                  lineHeight="2.5rem"
+                  h="3rem"
+                  paddingX={2}
+                  paddingY={1}
+                  rounded="md"
+                  _hover={{ cursor: 'pointer', bg: '#292929' }}
+                  fontSize="lg"
+                >
+                  {result.title}
+                </Box>
+              );
+            })}
+          </VStack>
+        )}
       </div>
 
       <div className="mt-5">
@@ -98,7 +159,7 @@ const Home = () => {
           <GalaxyGraph
             nodes={data.nodes}
             links={data.links}
-            searchResults={searchResults}
+            searchResults={serachIds}
           />
         )}
       </div>
