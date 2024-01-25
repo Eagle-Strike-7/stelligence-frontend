81ce2b7b930154d3f4b1daaffc713fc3fb453797
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-25 19:50:11 +0900

    [#76] refactor: Error,Loading 컴포넌트 분리

---
+++ src/app/(home)/components/LoadingComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingComponent = () => {
+  return (
+    <div className="h-screen bg-black text-white text-center pt-10">
+      그래프 로딩중
+    </div>
+  );
+};
+
+export default LoadingComponent;
