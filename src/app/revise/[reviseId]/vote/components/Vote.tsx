3227d05696d5895b44bcd1b1846ec4b9549553da
LeeDahee <dduosy9@gmail.com>
2024-01-28 16:42:12 +0900

    [#102] style: 레이아웃 설정

--- src/app/revise/[reviseId]/vote/components/Vote.tsx
+++ src/app/revise/[reviseId]/vote/components/Vote.tsx
@@ -14,25 +14,27 @@ const Vote = ({
   disAgreeNum: number;
 }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <h1>투표하기</h1>
-      <div>
-        <span>{agreePercent}%</span>
-        <span>{disAgreePercent}%</span>
+      <div className="flex flex-col gap-1">
+        <div className="flex flex-row justify-between">
+          <span>{agreePercent}%</span>
+          <span>{disAgreePercent}%</span>
+        </div>
+        <Progress
+          colorScheme="blue"
+          value={50}
+          size="lg"
+          sx={{
+            borderRadius: '1rem',
+          }}
+        />
+        <div className="flex flex-row justify-between">
+          <span>찬성({agreeNum}표)</span>
+          <span>반대({disAgreeNum}표)</span>
+        </div>
       </div>
-      <Progress
-        colorScheme="blue"
-        value={50}
-        size="lg"
-        sx={{
-          borderRadius: '1rem',
-        }}
-      />
-      <div>
-        <span>찬성({agreeNum}표)</span>
-        <span>반대({disAgreeNum}표)</span>
-      </div>
-      <div>
+      <div className="flex flex-row gap-4 justify-center">
         <Button leftIcon={<FaRegThumbsUp />}>찬성</Button>
         <Button leftIcon={<FaRegThumbsDown />}>반대</Button>
       </div>
