e52e555b333d6d3744f65f89a8da59487f4a79d4
LeeDahee <dduosy9@gmail.com>
2024-01-28 22:31:03 +0900

    [#107] style: 글자 크기 조정

--- src/app/debateList/components/CreateComment.tsx
+++ src/app/debateList/components/CreateComment.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CreateComment = () => {
   return (
     <div className="flex flex-col w-full mb-6 ">
-      <span className="w-32 text-xl font-bold mt-3 flex-shrink-0">댓글</span>
+      <span className="w-32 text-lg font-bold mt-3 flex-shrink-0">댓글</span>
       <Textarea
         className="w-full my-3 flex-shrink-0 bg-gray-50"
         placeholder="댓글을 여기에 입력해주세요 :)"
