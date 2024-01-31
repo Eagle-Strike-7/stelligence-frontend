d1c305a76b3259a8a4713ffe3117b15443540350
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-01-31 17:41:51 +0900

    fix: .env 변수 이름 변경(NEXT_PUBLIC 제거)

--- src/service/graph/getGraphData.ts
+++ src/service/graph/getGraphData.ts
@@ -6,12 +6,9 @@ import { Graph } from '@/types/graph/GraphProps';
 import axios from 'axios';
 
 const getGraphData = async (): Promise<Graph> => {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/documents`,
-    {
-      params: { depth: 3 },
-    },
-  );
+  const response = await axios.get(`${process.env.SERVER_URL}/api/documents`, {
+    params: { depth: 3 },
+  });
   const { data } = response;
   const transformedResults = transformResults(data.results.documentNodes);
   const transformedLinks = transformLinks(data.results.links);
