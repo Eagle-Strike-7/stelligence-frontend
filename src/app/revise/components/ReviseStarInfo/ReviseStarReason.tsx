a7899cce0c696f952aa342e04d6858b1dd750500
moonjw0 <121412353+moonjw0@users.noreply.github.com>
2024-02-01 23:31:04 +0900

    [#106] fix: 제목 태그 수정

--- src/app/revise/components/ReviseStarInfo/ReviseStarReason.tsx
+++ src/app/revise/components/ReviseStarInfo/ReviseStarReason.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ReviseStarReason = () => {
   return (
     <div className="flex flex-col w-full mt-4">
-      <div className="text-md font-bold mb-2">수정 이유</div>
+      <h3 className="text-md font-bold mb-2">수정 이유</h3>
       <textarea
         className="w-full h-28 p-4 border border-gray-300 rounded-md resize-none"
         placeholder="수정 이유를 입력하세요"
