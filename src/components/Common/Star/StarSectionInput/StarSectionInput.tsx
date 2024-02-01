7726a561890312142bac5d79a706110839e724ab
moonjw0 <jjwon1230@naver.com>
2024-02-02 05:58:56 +0900

    [#89] feat: 에디터 내용 입력 기능 구현

--- src/components/Common/Star/StarSectionInput/StarSectionInput.tsx
+++ src/components/Common/Star/StarSectionInput/StarSectionInput.tsx
@@ -4,18 +4,26 @@ import React from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import { StarterKit } from '@tiptap/starter-kit';
 // import Underline from '@tiptap/extension-underline';
+import { StarProps } from '@/types/newStar/newStarProps';
 import FixedMenu from './FixedMenu';
 
 // NOTE : 글 전체/섹션의 내용을 입력받는 컴포넌트 (TipTap)
-const StarSectionInput = () => {
+const StarSectionInput = ({ star, setStar }: StarProps) => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: '<p>Hello World! 🌎️</p>',
+    onUpdate: () => {
+      if (editor) {
+        const htmlContent = editor.getHTML();
+        setStar({ ...star, content: htmlContent });
+      }
+    },
   });
 
   const logContent = () => {
     if (editor) {
       const htmlContent = editor.getHTML();
+      // setStar({ ...star, content: htmlContent});
       console.log(htmlContent);
     }
   };
@@ -24,6 +32,7 @@ const StarSectionInput = () => {
     <div className="border border-gray-300 rounded-md">
       {editor && <FixedMenu editor={editor} />}
       <EditorContent editor={editor} className="min-h-80" />
+
       <button type="button" onClick={logContent}>
         Log Content
       </button>
