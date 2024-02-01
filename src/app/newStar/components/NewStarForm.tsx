8177518633025c6e1bd378dec8e05ffb3d53dfb1
moonjw0 <jjwon1230@naver.com>
2024-02-02 05:59:25 +0900

    [#89] feat: 새로운 별(문서) 생성 기능 구현

--- src/app/newStar/components/NewStarForm.tsx
+++ src/app/newStar/components/NewStarForm.tsx
@@ -6,22 +6,45 @@ import StarTitleInput from '@/components/Common/Star/StarTitleInput';
 import StarTagInput from '@/components/Common/Star/StarTagInput';
 import { Star } from '@/types/newStar/newStarProps';
 import AccentButton from '@/components/Common/Button/AccentButton';
+import axios from 'axios';
 
 const NewStarForm = () => {
   const [newStar, setNewStar] = useState<Star>({
     title: '',
-    tag: '',
+    documentId: 0,
     content: 'hello',
   });
 
+  const postNewStar = async (star: Star) => {
+    const tempUrl =
+      'http://ec2-43-203-87-227.ap-northeast-2.compute.amazonaws.com/api/documents';
+
+    try {
+      const response = await axios.post<Star>(tempUrl, JSON.stringify(star), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log('response', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const startsWithH2orH3 = /^<(h2|h3)>/i;
     if (newStar.title === '') {
       alert('제목을 입력해주세요');
-    } else if (newStar.tag === '') {
-      alert('상위 계층 태그를 연결해주세요');
     } else if (newStar.content === '') {
       alert('본문을 입력해주세요');
+    } else if (startsWithH2orH3.test(newStar.content) === false) {
+      alert(
+        '본문은 소제목으로 시작해야 합니다.\n##, ###을 통해 소제목을 생성해주세요.',
+      );
+    } else {
+      console.log(newStar);
+      postNewStar(newStar);
     }
   };
 
@@ -32,7 +55,7 @@ const NewStarForm = () => {
 
       <div className="flex flex-col w-full">
         <div className="text-md font-bold mb-2">본문</div>
-        <StarSectionInput />
+        <StarSectionInput star={newStar} setStar={setNewStar} />
       </div>
 
       <AccentButton name="생성하기" />
