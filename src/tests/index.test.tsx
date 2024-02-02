50bb2d6b00088e619cafffdb4c4a3b0fb2b40877
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-02-02 17:35:22 +0900

    [#125] test: 토론 목록 페이지 테스트 코드 작성

--- src/tests/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import Page from '@/app/debateList/page';
-import { render } from '@testing-library/react';
-
-describe('debateList', () => {
-  it('renders a heading', () => {
-    render(<Page />);
-  });
-});
