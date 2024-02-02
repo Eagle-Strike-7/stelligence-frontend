8c977a4ee5ee5232cc3e60f2e06b55da5bd355c3
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-02-03 00:34:22 +0900

    [#127] refactor: 토론 페이지 select 공통 컴포넌트로 분리

--- src/app/debateList/page.tsx
+++ src/app/debateList/page.tsx
@@ -7,37 +7,34 @@ import Wrapper from '@/components/Common/Wrapper';
 import { dummyDebateList } from '@/constants/dummyData';
 import React, { useState } from 'react';
 import { Pagination } from '@mui/material';
-import { Center, Select } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
 import Link from 'next/link';
+import ChakraSelect from '@/components/Common/ChakraSelect';
 
 const Page = () => {
   const [activeTab, setActiveTab] = useState<string>('진행중');
+  const [selectedOption, setSelectedOption] = useState<string>('최신순');
+  const options = [
+    { value: '최신순', label: '최신순' },
+    { value: '최근 댓글 순', label: '최근 댓글 순' },
+  ];
 
   // activeTab에 따라서 다른 axios 호출
+
+  // selectedOption 따라서 다른 axios 호출
+  console.log(selectedOption);
+
   return (
     <Wrapper>
       <PageTitle pageTitle="토론" />
-      <div className="flex justify-between w-full items-center my-0">
+      <div className="flex justify-between w-full items-center">
         <StateTab
           tab1="진행중"
           tab2="완료"
           activeTab={activeTab}
           setActiveTab={setActiveTab}
         />
-        <div className="flex my-0">
-          <Select
-            variant="filled"
-            size="sm"
-            rounded="md"
-            borderColor="gray.200"
-            borderWidth={1}
-            focusBorderColor="gray.400"
-            bg="white"
-          >
-            <option value="최신순">최신순</option>
-            <option value="최근댓글순">최근댓글순</option>
-          </Select>
-        </div>
+        <ChakraSelect options={options} setSelectedOption={setSelectedOption} />
       </div>
       {dummyDebateList.map(item => {
         return (
