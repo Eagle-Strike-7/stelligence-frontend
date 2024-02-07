607d14991a1ae479e996b2dce2a5e0d2e74ecb4f
moonjw0 <jjwon1230@naver.com>
2024-02-08 01:20:00 +0900

    [#151] fix: 150-글 수정요청에서 수정예정

--- src/app/stars/[starId]/revise/components/ReviseStarForm.tsx
+++ src/app/stars/[starId]/revise/components/ReviseStarForm.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { useState } from 'react';
-import { NewStar } from '@/types/star/NewStarProps';
-import StarTitleInput from '@/components/Common/Star/StarTitleInput';
-import StarTagInput from '@/components/Common/Star/StarTagInput';
+// import React, { useState } from 'react';
+// import { NewStar } from '@/types/star/NewStarProps';
+// import StarTitleInput from '@/components/Common/Star/StarTitleInput';
+// import StarTagInput from '@/components/Common/Star/StarTagInput';
 import SubmitButton from '@/components/Common/Button/SubmitButton';
 import ReviseStarReason from './ReviseStarInfo/ReviseStarReason';
 import ReviseStarContent from './ReviseStarContent/ReviseStarContent';
@@ -12,18 +12,18 @@ import ReviseStarDebate from './ReviseStarInfo/ReviseStarDebate';
 // NOTE : 수정요청 패이지
 // FIXME : 글 정보 묶기
 const ReviseStarForm = () => {
-  const [reviseStar, setReviseStar] = useState<NewStar>({
-    title: '',
-    parentDocumentId: 0,
-    realtedDebate: '#1234',
-    content: 'hello',
-  });
+  // const [reviseStar, setReviseStar] = useState<NewStar>({
+  //   title: '',
+  //   parentDocumentId: 0,
+  //   realtedDebate: '#1234',
+  //   content: 'hello',
+  // });
 
   return (
     <form className="flex flex-col w-full pt-5 px-32">
-      <StarTitleInput star={reviseStar} setStar={setReviseStar} />
+      {/* <StarTitleInput star={reviseStar} setStar={setReviseStar} /> */}
       <div className="flex flex-row">
-        <StarTagInput star={reviseStar} setStar={setReviseStar} />
+        {/* <StarTagInput star={reviseStar} setStar={setReviseStar} /> */}
         <ReviseStarDebate />
       </div>
 
