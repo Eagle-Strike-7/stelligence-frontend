4d6900d79812e5644579ce70129a9ba44384dc8c
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-02-07 17:09:40 +0900

    [#139] refactor: 폴더 이름 변경(카멜케이스->케밥케이스)

--- src/app/debate-list/page.tsx
+++ src/app/debate-list/page.tsx
@@ -9,7 +9,7 @@ import Wrapper from '@/components/Common/Wrapper';
 import PageTitle from '@/components/Common/PageTitle';
 import StateTab from '@/components/Common/StateTab';
 import ChakraSelect from '@/components/Common/ChakraSelect';
-import DebateListCard from '@/app/debateList/components/DebateListCard';
+import DebateListCard from '@/app/debate-list/components/DebateListCard';
 
 interface Debate {
   debateId: number;
