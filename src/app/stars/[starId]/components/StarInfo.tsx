2d41621d516a4a550472d0b830d2105ee88a6293
moonjw0 <jjwon1230@naver.com>
2024-02-09 05:28:27 +0900

    [#150] feat: 수정요청페이지로 연결하는 기능, 기존 컴포넌트 정리

--- src/app/stars/[starId]/components/StarInfo.tsx
+++ src/app/stars/[starId]/components/StarInfo.tsx
@@ -2,6 +2,7 @@ import PageTitle from '@/components/Common/PageTitle';
 import { Button } from '@chakra-ui/react';
 import { AiOutlineEllipsis } from 'react-icons/ai';
 import { LuBookmark } from 'react-icons/lu';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface StarInfoProps {
   title: string;
@@ -12,6 +13,19 @@ interface StarInfoProps {
 // NOTE : 글의 정보를 보여주는 컴포넌트
 // FIXME : 최종 수정 일시 추가, 편집중 상태 추가
 const StarInfo = ({ title, lastModifiedAt, editable }: StarInfoProps) => {
+  const pathname = usePathname();
+  const documentId = pathname.split('/').pop();
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    alert('투표중인 글입니다.');
+  };
+
+  const handleEdit = () => {
+    router.push(`/stars/${documentId}/revise`);
+  };
+
   return (
     <div className="flex flex-row w-full justify-between">
       <div className="flex flex-col">
@@ -30,15 +44,23 @@ const StarInfo = ({ title, lastModifiedAt, editable }: StarInfoProps) => {
             <AiOutlineEllipsis size="1.5rem " />
           </Button>
         </div>
-        {editable && (
+        {editable === false ? ( // TODO : 버튼 수정 및 정리하기
           <Button
             size="md"
+            colorScheme="accent"
             variant="solid"
-            colorScheme="blue"
-            background="accent.500"
-            mr="0.5rem"
+            onClick={handleClick}
           >
-            편집중
+            투표중
+          </Button>
+        ) : (
+          <Button
+            size="md"
+            colorScheme="gray"
+            variant="outline"
+            onClick={handleEdit}
+          >
+            편집
           </Button>
         )}
       </div>
