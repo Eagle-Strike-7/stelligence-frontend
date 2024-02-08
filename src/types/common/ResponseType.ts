4d87b57cea87c9c331fe0f0889f61fd1e5733dbb
moonjw0 <jjwon1230@naver.com>
2024-02-09 05:29:16 +0900

    [#150] feat/style: 수정요청 타입 정의, 스타일 정의

--- src/types/common/ResponseType.ts
+++ src/types/common/ResponseType.ts
@@ -9,3 +9,18 @@ export interface StarResponseType<T> {
   message: string;
   results: T;
 }
+
+export enum Heading {
+  H1 = 'H1',
+  H2 = 'H2',
+  H3 = 'H3',
+  H4 = 'H4',
+  H5 = 'H5',
+  H6 = 'H6',
+}
+
+export enum WriteType {
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
+  CREATE = 'CREATE',
+}
