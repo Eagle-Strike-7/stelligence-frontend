cdd4ac9e3d61bb5cda526e35515bd0da6657f6a0
LeeDahee <dduosy9@gmail.com>
2024-02-08 11:58:18 +0900

    [#156] style: 투표 상세페이지 디자인 변경

--- src/app/vote-list/[voteId]/page.tsx
+++ src/app/vote-list/[voteId]/page.tsx
@@ -1,36 +1,52 @@
 import BeforeAfter from '@/components/Common/BeforeAfter';
 import Wrapper from '@/components/Common/Wrapper';
-import { Card } from '@chakra-ui/react';
+import { Card, Input } from '@chakra-ui/react';
 import React from 'react';
 import ReviseInformation from '@/app/vote-list/[voteId]/components/ReviseInformation';
 import Vote from './components/Vote';
 
 // FIXME 백엔드 통신 이후 삭제
 const dummyReviseData = {
-  documentTitle: '마리모',
+  contributeId: 2,
   contributeTitle: '마리모에 대한 전반적인 수정 요청',
-  relatedDebateNum: '12345',
-  parentTitle: '식물',
-  contributorNickname: '독수리타법 7남매',
-  remainVoteTime: 120,
   contributeDescription:
-    '마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.',
-  contents: [
+    '마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.마리모는 동물입니다. 동물을 식물이라고 부르는 것은 마리모에게 실례입니다.',
+  contributeStatus: 'VOTING',
+  documentId: 1,
+  documentTitle: '마리모',
+  parentDocumentId: 456,
+  contributor: {
+    memberId: 101,
+    nickname: '독수리타법 7남매',
+    profileImgUrl: '',
+  },
+  amendments: [
     {
-      id: 1,
-      title: '마리모는 식물이 아닙니다.',
-      beforeContent:
-        '마리모(일본어: 毬藻; 라틴어: Aegagropila linnaei)는 공 모양의 집합체를 만드는 것으로 잘 알려져 있는 담수성 녹조류의 일종이다. 또한 아칸호의 마리모는 특히 아름다운 구상체를 만들며, 마리모는 일본의 천연기념물로 지정되어 있다./n그리고 마리모는 공 모양의 집합체를 형성하지만 그 구상체 하나가 곧 마리모 한 개체인 것이 아니고, 그 구상체를 구성하는 가는 섬유(사상체)가 마리모의 개체 단위이다. 많은 서식지에서 마리모는 구상체를 구성하지 않고 사상체의 형태로 산다. 겉보기에는 부드러울 것 같지만 실제로는 딱딱한 말(녹조)로, 만져보면 쿡쿡 찔리는 듯한 감촉이 있다.\n1897년 일본에서 삿포로 농학교(현 홋카이도 대학)의 카와카미 타키야가 아칸 호에서 발견한 형태를 더러 マリモ(毬藻), 즉 ‘둥근 마름풀’이라는 이름을 붙였다.',
-      afterContent: '마리모는 동물이다.',
-    },
-    {
-      id: 2,
-      title: '마리모는 귀엽습니다.',
-      beforeContent: '',
-      afterContent:
-        '마리모가 어항 위로 올라오는 행동은 광합성을 통해 생긴 기포에 의한 것이다. 요즘엔 다시 반려식물로 인기를 누리고 있다. 마트에 가면 볼 수 있는 반려식물중 하나이다. 또한 마리모는 기분이 좋으면 떠오르는데, 이때 소원을 빌면 이루어진다는 전설이 있다.',
+      amendmentId: 123,
+      type: 'UPDATE',
+      targetSection: {
+        // 수정 전
+        sectionId: 456,
+        revision: 8,
+        heading: 'H1',
+        title: '마리모는 식물이 아닙니다.',
+        content: '마리모는 동물이다.',
+      },
+      // 수정 후
+      requestedSectionHeading: 'H1',
+      requestedSectionTitle: '마리모는 식물입니다',
+      requestedSectionContent: '마리모는 식물입니다 절대 동물일 수가 없습니다',
+      creatingOrder: 1,
     },
   ],
+  beforeDocumentTitle: '가나다라마사아자차카타파하가나',
+  afterDocumentTitle: '가나다라마사아자차카타파하가나',
+  beforeParentDocumentId: 987,
+  beforeParentDocumentTitle: '녹조류',
+  afterParentDocumentId: 456,
+  afterParentDocumentTitle: '양서류',
+  endAt: '2024-05-15T09:32',
+  relatedDebateId: 45384,
 };
 
 const dummyVoteData = {
@@ -42,29 +58,107 @@ const dummyVoteData = {
 const Page = () => {
   return (
     <Wrapper>
-      <div className="px-32 pt-5 ">
-        {/* TODO 수정요청사항과 투표 영역 구분하기 - 고민하기 */}
-        <Card paddingY="2rem" paddingX="2rem">
-          <div className="flex flex-col gap-4">
-            {/* SECTION 수정요청 글 정보 영역 */}
-            <ReviseInformation reviseData={dummyReviseData} />
-
+      <div className="pt-5">
+        {/* TODO 페이지 제목 - 공통컴포넌트로 분리 */}
+        <div className="flex flex-col gap-1 mb-16">
+          <h1 className="text-3xl font-bold">투표하기</h1>
+          <p className="text-sm">수정요청 반영 여부에 대해 투표하세요! </p>
+        </div>
+        <div className="flex flex-col gap-8">
+          {/* SECTION 수정요청 글 정보 영역 */}
+          <ReviseInformation reviseData={dummyReviseData} />
+          <hr />
+          <div className="flex flex-col">
+            <h2 className="text-xl font-bold mb-4">수정 요청 사항</h2>
+            <div className="grid grid-cols-2 mb-10">
+              <p className="text-lg text-center text-gray-500">수정 전</p>
+              <p className="text-lg text-center text-gray-500">수정 후</p>
+            </div>
+            {/* SECTION 글 제목 */}
+            {dummyReviseData.beforeDocumentTitle ===
+              dummyReviseData.afterDocumentTitle || (
+              <div className="mb-6">
+                <h3 className="text font-bold">글 제목</h3>
+                <div className="grid grid-cols-2 mt-2 gap-4">
+                  <Input
+                    isDisabled
+                    value={dummyReviseData.beforeDocumentTitle}
+                    bg="white"
+                    textAlign="center"
+                    paddingY="1.5rem"
+                    sx={{
+                      _disabled: {
+                        color: 'black',
+                      },
+                    }}
+                  />
+                  <Input
+                    isDisabled
+                    value={dummyReviseData.afterDocumentTitle}
+                    bg="white"
+                    textAlign="center"
+                    paddingY="1.5rem"
+                    sx={{
+                      _disabled: {
+                        color: 'black',
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+            {dummyReviseData.beforeParentDocumentTitle ===
+              dummyReviseData.afterParentDocumentTitle || (
+              <div>
+                <h3 className="text font-bold">상위 계층 태그</h3>
+                <div className="grid grid-cols-2 mt-2 gap-4">
+                  <Input
+                    isDisabled
+                    value={dummyReviseData.beforeParentDocumentTitle}
+                    bg="white"
+                    textAlign="center"
+                    paddingY="1.5rem"
+                    sx={{
+                      _disabled: {
+                        color: 'black',
+                      },
+                    }}
+                  />
+                  <Input
+                    isDisabled
+                    value={dummyReviseData.afterParentDocumentTitle}
+                    bg="white"
+                    textAlign="center"
+                    paddingY="1.5rem"
+                    sx={{
+                      _disabled: {
+                        color: 'black',
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+            {/* SECTION 상위 계층 태그 */}
             {/* SECTION 수정요청 사항 내용 영역 */}
-            <div className="flex flex-col gap-16 mt-10">
-              {dummyReviseData.contents.map((content, index) => {
+            <div className="flex flex-col gap-16 mt-16">
+              {dummyReviseData.amendments.map((amendment, index) => {
                 return (
                   <BeforeAfter
-                    key={content.id}
+                    key={amendment.amendmentId}
                     index={index}
-                    title={content.title}
-                    before={content.beforeContent}
-                    after={content.afterContent}
+                    beforeHeading={amendment.targetSection.heading}
+                    afterHeading={amendment.requestedSectionHeading}
+                    beforeTitle={amendment.targetSection.title}
+                    afterTitle={amendment.requestedSectionTitle}
+                    beforeContent={amendment.targetSection.content}
+                    afterContent={amendment.requestedSectionContent}
                   />
                 );
               })}
             </div>
           </div>
-        </Card>
+        </div>
         {/* SECTION 투표 영역 */}
         <div className="mt-16">
           <Card padding="2rem">
