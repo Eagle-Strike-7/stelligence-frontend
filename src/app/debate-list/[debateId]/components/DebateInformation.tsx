23b4e04affc056b4a86916728e19c2476a00a27c
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-02-10 02:04:35 +0900

    [#167] fix: 정보 렌더링 순서 변경

--- src/app/debate-list/[debateId]/components/DebateInformation.tsx
+++ src/app/debate-list/[debateId]/components/DebateInformation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LabelText from '@/components/Common/LabelText';
+import { Tag } from '@chakra-ui/react';
 import { Debate } from '../page.server';
 
 // TODO ReviseInformation과 통일
@@ -7,18 +8,17 @@ const DebateInformation = ({ debateData }: { debateData: Debate }) => {
   return (
     <div className="flex flex-col gap-8">
       <div className="grid grid-cols-2 gap-4">
-        <LabelText label="글 제목" text={debateData.documentTitle || '제목'} />
-        <LabelText
-          label="상위 계층 태그"
-          text={`${debateData.documentId}` || '상위 계층 태그'}
-        />
-      </div>
-      <div className="grid grid-cols-2 gap-4">
         <LabelText
           label="수정 요청안 제목"
           text={debateData.contributeTitle || '수정요청안 제목'}
         />
-        <LabelText label="남은 투표 시간" text="sss" />
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <LabelText label="글제목" text="sss" />
+        <LabelText
+          label="상위 계층 태그"
+          text={`${debateData.documentId}` || '상위 계층 태그'}
+        />
       </div>
 
       <div className="grid grid-cols-2 gap-4">
@@ -26,14 +26,15 @@ const DebateInformation = ({ debateData }: { debateData: Debate }) => {
           label="수정 요청자"
           text={debateData.contributor?.nickname || '수정요청자'}
         />
-        <LabelText
-          label="연관된 토론 번호"
-          text={`#${debateData.documentId || '연관된 토론 번호'}`}
-        />
+        <div className="flex">
+          <h3 className="font-bold text-md w-36">남은토론시간</h3>
+          <Tag className="text-md text-black">2시간</Tag>
+        </div>
       </div>
+
       <LabelText
         label="수정 요청 이유"
-        text={debateData.contributeDescription || '수정 요청 이유'}
+        text="asfadskfjlsadjflskajdflsajfdlskajdlkjslkfjdlskjflskdjflksjlkssfljsdlfkjalskdfjsaljflsjflksjdflksj sdjflksjflsjdlfsjflkdsjl"
       />
     </div>
   );
