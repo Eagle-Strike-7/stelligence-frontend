640f474fdd3b08e4c77ddf1ba98d5eaae5775a79
LeeDahee <dduosy9@gmail.com>
2024-02-10 15:13:32 +0900

    [#135] feat: 수정요청 글 정보 영역 데이터 연결, 남은 투표시간 계산

--- src/app/vote-list/[voteId]/components/ReviseInformation.tsx
+++ src/app/vote-list/[voteId]/components/ReviseInformation.tsx
@@ -1,104 +1,60 @@
 import React from 'react';
 import LabelText from '@/components/Common/LabelText';
-
-// TODO - type, heading, status는 열거형 or 유니온 타입으로 변경
-/*
-export const Type = {
-  update: 'UPDATE',
-  create: 'CREATE',
-  delete: 'DELETE',
-} as const;
-export const Heading = {
-  h1: 'H1',
-  h2: 'H2',
-  h3: 'H3',
-  h4: 'H4',
-  h5: 'H5',
-  h6: 'H6',
-} as const;
-export const Status = {
-  voting: 'VOTING',
-  merged: 'MERGED',
-  rejected: 'REJECTED',
-  debating: 'DEBATING',
-} as const;
-*/
-
-// NOTE - 수정요청 글의 정보 type
-interface AmendmentData {
-  amendmentId: number;
-  type: string;
-  targetSection: {
-    sectionId: number;
-    revision: number;
-    heading: string;
-    title: string;
-    content: string;
-  };
-  requestedSectionHeading: string;
-  requestedSectionTitle: string;
-  requestedSectionContent: string;
-  creatingOrder: number;
-}
-interface ReviseDataResponse {
-  contributeId: number;
-  contributeTitle: string;
-  contributeDescription: string;
-  contributeStatus: string;
-  documentId: number;
-  documentTitle: string;
-  parentDocumentId: number;
-  contributor: {
-    memberId: number;
-    nickname: string;
-    profileImgUrl: string;
-  };
-  amendments: AmendmentData[];
-  beforeDocumentTitle: string;
-  afterDocumentTitle: string;
-  beforeParentDocumentId: number;
-  beforeParentDocumentTitle: string;
-  afterParentDocumentId: number;
-  afterParentDocumentTitle: string;
-  endAt: string;
-  relatedDebateId: number;
-}
+import { ReviseDataResponse } from '@/service/vote/voteService';
 
 const ReviseInformation = ({
   reviseData,
 }: {
   reviseData: ReviseDataResponse;
 }) => {
+  const calculateRemainTime = () => {
+    const now = new Date();
+    const endAt = new Date(reviseData.results.endAt);
+    const remainTime = endAt.getTime() - now.getTime();
+    if (!Number.isNaN(endAt.getTime()) && remainTime >= 0) {
+      const minutes = Math.floor(remainTime / 60 / 1000);
+      const hours = Math.floor(minutes / 60);
+      const days = Math.floor(hours / 24);
+
+      return `${days}일 ${hours % 24}시간 ${minutes % 60}분`;
+    } return '0일 0시간 0분';
+  };
+
+  console.log(new Date(reviseData.results.endAt));
+
   return (
     <div className="flex flex-col gap-8">
       <div className="grid grid-cols-2 gap-4">
-        <LabelText label="글 제목" text={reviseData.documentTitle || '제목'} />
+        <LabelText
+          label="글 제목"
+          text={reviseData?.results.documentTitle || '제목'}
+        />
         <LabelText
           label="상위 계층 태그"
-          text={`${reviseData.parentDocumentId}` || '상위 계층 태그'}
+          text={reviseData?.results.parentDocumentId || '없음'}
         />
       </div>
       <div className="grid grid-cols-2 gap-4">
         <LabelText
           label="수정 요청안 제목"
-          text={reviseData.contributeTitle || '수정요청안 제목'}
+          text={reviseData?.results.contributeTitle || '수정요청안 제목'}
         />
-        <LabelText label="남은 투표 시간" text="sss" />
+        <LabelText label="남은 투표 시간" text={calculateRemainTime()} />
       </div>
 
       <div className="grid grid-cols-2 gap-4">
         <LabelText
           label="수정 요청자"
-          text={reviseData.contributor?.nickname || '수정요청자'}
+          text={reviseData?.results.contributor.nickname || '수정요청자'}
         />
         <LabelText
           label="연관된 토론 번호"
-          text={`#${reviseData.relatedDebateId || '연관된 토론 번호'}`}
+          text={reviseData?.results.relatedDebateId || '없음'}
         />
       </div>
       <LabelText
         label="수정 요청 이유"
-        text={reviseData.contributeDescription || '수정 요청 이유'}
+        text={reviseData?.results.contributeDescription || '수정 요청 이유'}
       />
     </div>
   );
