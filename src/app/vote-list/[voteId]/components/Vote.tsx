46eb7e1b12fc4425f7301317bfdf2226a078d882
LeeDahee <dduosy9@gmail.com>
2024-02-10 15:17:02 +0900

    [#135] feat: 투표 하기 기능 구현

--- src/app/vote-list/[voteId]/components/Vote.tsx
+++ src/app/vote-list/[voteId]/components/Vote.tsx
@@ -1,48 +1,89 @@
+import { VoteResponse, postVote } from '@/service/vote/voteService';
 import { Button, Progress } from '@chakra-ui/react';
-import React from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import React, { useState } from 'react';
 import { FaRegThumbsDown, FaRegThumbsUp } from 'react-icons/fa';
 
 const Vote = ({
-  agreePercent,
-  disAgreePercent,
-  agreeNum,
-  disAgreeNum,
+  voteData,
+  contributeId,
 }: {
-  agreePercent: number;
-  disAgreePercent: number;
-  agreeNum: number;
-  disAgreeNum: number;
+  voteData: VoteResponse;
+  contributeId: number;
 }) => {
+  const [myVote, setMyVote] = useState<boolean | null>(
+    voteData?.results.myVote,
+  );
+  const queryClient = useQueryClient();
+  const voteMutation = useMutation<
+    AxiosResponse,
+    Error,
+    { contributeId: number; isAgree: boolean }
+  >({
+    mutationFn: postVote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['vote', contributeId] });
+    },
+    onError: (error: Error) => {
+      console.error('투표 하기 실패: ', error);
+    },
+  });
+
+  const handleVote = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const button = e.currentTarget as HTMLButtonElement;
+    const isAgree: boolean = button.dataset.vote === 'agree';
+    if (myVote !== isAgree) {
+      voteMutation.mutate({ contributeId, isAgree });
+    } else {
+      // TODO 이미 선택한 버튼을 클릭 시 버튼 배경색 기본색으로 변경
+      setMyVote(null);
+    }
+  };
+
+  const calculateAgreePercent = () => {
+    const agree = voteData?.results.agreeCount;
+    const disagree = voteData?.results.disagreeCount;
+    if (agree === 0 && disagree === 0) return 0;
+    return Math.floor((agree / (agree + disagree)) * 100);
+  };
   return (
     <div className="flex flex-col gap-3">
       <h1 className="text-xl font-bold mb-3">투표하기</h1>
       <div className="flex flex-col gap-1">
         <div className="flex flex-row justify-between">
-          <span className="text-lg font-bold">{agreePercent}%</span>
-          <span className="text-lg font-bold">{disAgreePercent}%</span>
+          <span className="text-lg font-bold">{calculateAgreePercent()}%</span>
+          <span className="text-lg font-bold">
+            {100 - calculateAgreePercent()}%
+          </span>
         </div>
         <Progress
           bg="red.500"
           colorScheme="blue"
-          value={agreePercent}
+          value={calculateAgreePercent()}
           height="2rem"
           sx={{
             borderRadius: '1rem',
           }}
         />
         <div className="flex flex-row justify-between">
-          <span className="font-bold">찬성({agreeNum}표)</span>
-          <span className="font-bold">반대({disAgreeNum}표)</span>
+          <span className="font-bold">
+            찬성({voteData?.results.agreeCount}표)
+          </span>
+          <span className="font-bold">
+            반대({voteData?.results.disagreeCount}표)
+          </span>
         </div>
       </div>
       <div className="flex flex-row gap-4 justify-center">
-        {/* TODO 선택 시 색상 변경 */}
         <Button
           leftIcon={<FaRegThumbsUp />}
           _hover={{
             bg: 'blue.500',
             color: 'white',
           }}
+          data-vote="agree"
+          onClick={handleVote}
         >
           찬성
         </Button>
@@ -52,6 +93,8 @@ const Vote = ({
             bg: 'red.500',
             color: 'white',
           }}
+          data-vote="disagree"
+          onClick={handleVote}
         >
           반대
         </Button>
