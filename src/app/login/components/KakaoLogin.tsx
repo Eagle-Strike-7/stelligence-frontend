22e81fdc3fb45c5dc4088c28698d1931afeb051a
LeeDahee <dduosy9@gmail.com>
2024-02-11 18:32:58 +0900

    [#169] fix: 소셜로그인 서버 url 변경

--- src/app/login/components/KakaoLogin.tsx
+++ src/app/login/components/KakaoLogin.tsx
@@ -5,8 +5,7 @@ import { Button } from '@chakra-ui/react';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 
 const KakaoLogin = () => {
-  // NOTE 로그인 전용 서버로 연결 (추후 기존 서버로 변경 예정)
-  const link = `${process.env.NEXT_PUBLIC_SERVER_URL}:8080/oauth2/authorization/kakao`;
+  const link = `${process.env.NEXT_PUBLIC_SERVER_URL}/oauth2/authorization/kakao`;
   const handleClick = () => {
     window.location.href = link;
   };
