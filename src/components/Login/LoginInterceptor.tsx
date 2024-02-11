2e5dd0b459f637fa8db85452d56ead1b51d45801
LeeDahee <dduosy9@gmail.com>
2024-02-11 18:29:40 +0900

    [#169] feat: axios 인터셉터 파일 분리, withCredential 설정

---
+++ src/components/Login/LoginInterceptor.tsx
@@ -0,0 +1,39 @@
+import apiClient from '@/service/login/axiosClient';
+import { useToast } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+const LoginInterceptor = () => {
+  const toast = useToast();
+  const router = useRouter();
+
+  useEffect(() => {
+    const interceptorId = apiClient.interceptors.response.use(
+      response => {
+        return response;
+      },
+      error => {
+        // SECTION UnAuthorized: 로그인하지 않은 사용자가 접속했을 때
+        if (error.response && error.response.status === 401) {
+          toast({
+            title: '로그아웃 되었습니다.',
+            description: '다시 로그인 해주세요🚀',
+            status: 'warning',
+            duration: 1000,
+            isClosable: true,
+          });
+          router.push('/login');
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      apiClient.interceptors.response.eject(interceptorId);
+    };
+  }, [router]);
+  return null;
+};
+
+export default LoginInterceptor;
--- src/service/login/axiosClient.ts
+++ src/service/login/axiosClient.ts
@@ -1,25 +1,8 @@
 import axios from 'axios';
-import { useRouter } from 'next/router';
 
 const apiClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  withCredentials: true,
 });
-
-apiClient.interceptors.response.use(
-  response => {
-    return response;
-  },
-  error => {
-    // SECTION UnAuthorized: 로그인하지 않은 사용자가 접속했을 때
-    if (error.response.status === 401) {
-      useRouter().push('/login');
-    }
-
-    return Promise.reject(error);
-  },
-);
 
 export default apiClient;
