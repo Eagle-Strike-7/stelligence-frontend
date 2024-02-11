3aab77affa590bd3b4029e61ec0fe017d3b3f02b
LeeDahee <dduosy9@gmail.com>
2024-02-11 20:08:29 +0900

    [#169] fix: api 인스턴스에 content type 추가

--- src/service/login/axiosClient.ts
+++ src/service/login/axiosClient.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
 });
 
