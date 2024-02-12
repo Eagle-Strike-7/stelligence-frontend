5ae242458f7914e4a81758259e36e9b1b9b4ee3f
moonjw0 <jjwon1230@naver.com>
2024-02-13 06:05:57 +0900

    [#194] style: 버튼 다크모드 적용

--- src/app/stars/[starId]/revise/components/ReviseStarSectionShow.tsx
+++ src/app/stars/[starId]/revise/components/ReviseStarSectionShow.tsx
@@ -38,7 +38,7 @@ const ReviseStarSectionShow = ({
   };
 
   return (
-    <div className="flex flex-col w-full mb-6">
+    <div className="flex flex-col w-full my-6">
       <div className="flex flex-row justify-between items-center justify-items-center mb-2">
         <span className={`${headSize} text-white font-bold align-middle`}>
           {title}
@@ -47,7 +47,7 @@ const ReviseStarSectionShow = ({
           <Button
             size="md"
             color="white"
-            colorScheme="gray"
+            _hover={{ bg: '#ebedf0', textColor: 'black', fontWeight: 600 }}
             variant="outline"
             h="2rem"
             onClick={handleEditClick}
@@ -57,7 +57,7 @@ const ReviseStarSectionShow = ({
           <Button
             size="md"
             color="white"
-            colorScheme="gray"
+            _hover={{ bg: '#ebedf0', textColor: 'black', fontWeight: 600 }}
             variant="outline"
             h="2rem"
             onClick={handleDeleteClick}
