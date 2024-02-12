378e8a3a92f9f3c3add21405862435259a9256a1
LeeDahee <dduosy9@gmail.com>
2024-02-13 01:17:36 +0900

    [#157] style: 공통컴포넌트 - 연관 토론 번호 기능 추가

--- src/components/Common/PageTitleDescription.tsx
+++ src/components/Common/PageTitleDescription.tsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import { Tag, TagLabel } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+interface PageTitleDescriptionProps {
+  title: string;
+  description: string;
+  relatedDebateId?: number;
+}
 
 const PageTitleDescription = ({
   title,
   description,
-}: {
-  title: string;
-  description: string;
-}) => {
+  relatedDebateId,
+}: PageTitleDescriptionProps) => {
+  const [relatedDebateIdNum, setRelatedDebateIdNum] = useState<string>('');
+  const router = useRouter();
+  const handleClickRelatedDebate = () => {
+    if (relatedDebateId) {
+      router.push(`/debate-list/${relatedDebateId}`);
+    }
+  };
+
+  useEffect(() => {
+    const id = relatedDebateId ?? '없음';
+    setRelatedDebateIdNum(id.toString());
+  }, []);
+
   return (
     <div className="flex flex-col gap-1 text-white w-max ml-2 mb-12">
-      <h1 className="text-4xl font-bold">{title}</h1>
-
+      <div className="flex">
+        <h1 className="text-4xl font-bold w-fit">{title}</h1>
+        <Tag
+          size="md"
+          backgroundColor="white"
+          color="black"
+          height="fit-content"
+          alignSelf="center"
+          marginLeft="0.5rem"
+          paddingY="0.3rem"
+          onClick={handleClickRelatedDebate}
+          _hover={{
+            cursor: 'pointer',
+          }}
+        >
+          <TagLabel>💬 연관 토론: {relatedDebateIdNum}</TagLabel>
+        </Tag>
+      </div>
       <p className="text-sm">{description}</p>
     </div>
   );
