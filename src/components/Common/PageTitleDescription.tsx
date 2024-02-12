5d695a0b2db69868b30407d6c1c2316f68417454
LeeDahee <dduosy9@gmail.com>
2024-02-13 01:44:30 +0900

    [#157] style: 연관된 토론 번호가 없으면 tag 렌더링 되지 않게 변경

--- src/components/Common/PageTitleDescription.tsx
+++ src/components/Common/PageTitleDescription.tsx
@@ -1,6 +1,6 @@
 import { Tag, TagLabel } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 interface PageTitleDescriptionProps {
   title: string;
@@ -13,7 +13,6 @@ const PageTitleDescription = ({
   description,
   relatedDebateId,
 }: PageTitleDescriptionProps) => {
-  const [relatedDebateIdNum, setRelatedDebateIdNum] = useState<string>('');
   const router = useRouter();
   const handleClickRelatedDebate = () => {
     if (relatedDebateId) {
@@ -21,30 +20,27 @@ const PageTitleDescription = ({
     }
   };
 
-  useEffect(() => {
-    const id = relatedDebateId ?? '없음';
-    setRelatedDebateIdNum(id.toString());
-  }, []);
-
   return (
     <div className="flex flex-col gap-1 text-white w-max ml-2 mb-12">
       <div className="flex">
         <h1 className="text-4xl font-bold w-fit">{title}</h1>
-        <Tag
-          size="md"
-          backgroundColor="white"
-          color="black"
-          height="fit-content"
-          alignSelf="center"
-          marginLeft="0.5rem"
-          paddingY="0.3rem"
-          onClick={handleClickRelatedDebate}
-          _hover={{
-            cursor: 'pointer',
-          }}
-        >
-          <TagLabel>💬 연관 토론: {relatedDebateIdNum}</TagLabel>
-        </Tag>
+        {relatedDebateId && (
+          <Tag
+            size="md"
+            backgroundColor="white"
+            color="black"
+            height="fit-content"
+            alignSelf="center"
+            marginLeft="0.5rem"
+            paddingY="0.3rem"
+            onClick={handleClickRelatedDebate}
+            _hover={{
+              cursor: 'pointer',
+            }}
+          >
+            <TagLabel>💬 연관 토론: {relatedDebateId}</TagLabel>
+          </Tag>
+        )}
       </div>
       <p className="text-sm">{description}</p>
     </div>
