861a8ee4c0d13f5b34a675b666699fd0f2e259b0
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-02-14 15:29:47 +0900

    [#210] fix: 토론 상세페이지 조회 데이터 태그 및 예외 처리 수정

--- src/app/debate-list/[debateId]/components/DebateInformation.tsx
+++ src/app/debate-list/[debateId]/components/DebateInformation.tsx
@@ -1,42 +1,52 @@
 import React from 'react';
 import LabelText from '@/components/Common/LabelText';
 import { Tag } from '@chakra-ui/react';
-import { Debate } from '../page.server';
+import { Contribute } from '@/types/common/Amendment';
+import calculateRemainTime from '@/lib/calculateRemainTime';
 import MiddleTitle from './MiddleTitle';
 
 // TODO ReviseInformation과 통일
-const DebateInformation = ({ debateData }: { debateData: Debate }) => {
+const DebateInformation = ({
+  contributeData,
+}: {
+  contributeData: Contribute;
+}) => {
+  const leftDebateTime = calculateRemainTime(contributeData.endAt);
   return (
     <div className="flex flex-col gap-8 mb-4">
       <MiddleTitle title="개요" color="white" />
       <div className="grid grid-cols-2 gap-4">
         <LabelText
           label="수정 요청안 제목"
-          text={debateData.contributeTitle || '수정요청안 제목'}
+          text={contributeData.contributeTitle || 'X'}
         />
       </div>
       <div className="grid grid-cols-2 gap-4">
-        <LabelText label="글제목" text="sss" />
-        <LabelText
-          label="상위 계층 태그"
-          text={`${debateData.documentId}` || '상위 계층 태그'}
-        />
+        <LabelText label="글제목" text={contributeData.documentTitle} />
+        <div className="flex">
+          <h3 className="font-bold text-md w-36 text-white">상위계층태그</h3>
+          {contributeData.parentDocumentTitle ? (
+            <Tag className="text-md  text-black">
+              {contributeData.parentDocumentTitle}
+            </Tag>
+          ) : (
+            'X'
+          )}
+        </div>
       </div>
 
       <div className="grid grid-cols-2 gap-4">
         <LabelText
           label="수정 요청자"
-          text={debateData.contributor?.nickname || '수정요청자'}
+          text={contributeData.contributor.nickname || 'X'}
         />
-        <div className="flex">
-          <h3 className="font-bold text-md w-36 text-white">남은토론시간</h3>
-          <Tag className="text-md  text-black">2시간</Tag>
-        </div>
+
+        <LabelText label="남은 토론 시간" text={leftDebateTime || 'X'} />
       </div>
 
       <LabelText
         label="수정 요청 이유"
-        text="asfadskfjlsadjflskajdflsajfdlskajdlkjslkfjdlskjflskdjflksjlkssfljsdlfkjalskdfjsaljflsjflksjdflksj sdjflksjflsjdlfsjflkdsjl"
+        text={contributeData.contributeDescription || 'X'}
       />
     </div>
   );
