f7ad1de0c23a0c8f7def92d1c09bc80f0843b189
LeeDahee <dduosy9@gmail.com>
2024-02-15 22:02:44 +0900

    [#182] style: wrapper 세로 padding 0

--- src/components/Common/Wrapper.tsx
+++ src/components/Common/Wrapper.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Box, Center } from '@chakra-ui/react';
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <Center>
-            <Box w="70rem" className="mx-20 my-10">
-                {children}
-            </Box>
-        </Center>
-    );
+  return (
+    <Center>
+      <Box w="70rem" className="mx-20">
+        {children}
+      </Box>
+    </Center>
+  );
 };
 
 export default Wrapper;
