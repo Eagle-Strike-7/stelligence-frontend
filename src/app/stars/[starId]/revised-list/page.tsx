577e1fabaee43accfb4fda8c8b3790508da80fa7
LeeDahee <dduosy9@gmail.com>
2024-02-16 22:58:50 +0900

    [#248] feat: 특정 글 수정요청 목록 페이지 구현

---
+++ src/app/stars/[starId]/revised-list/page.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import VoteListCard from '@/app/vote-list/components/VoteListCard';
+import StateTab from '@/components/Common/StateTab';
+import PageTitleDescription from '@/components/Common/Title/PageTitleDescription';
+import Wrapper from '@/components/Common/Wrapper';
+import formatDate from '@/lib/formatDate';
+import getSpecificReviseData, {
+  SpecificReviseResponse,
+} from '@/service/revise/reviseService';
+import { Center } from '@chakra-ui/react';
+import { Pagination } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { useParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+const Page = () => {
+  const documentId = Number(useParams().starId);
+  const [activeTab, setActiveTab] = useState<string>('반영 완료');
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const { data: specificReviseData } = useQuery<
+    SpecificReviseResponse,
+    AxiosError
+  >({
+    queryKey: ['revise', activeTab],
+    queryFn: () =>
+      {return getSpecificReviseData({ documentId, activeTab, page: currentPage - 1 })},
+  });
+
+  useEffect(() => {
+    if (specificReviseData) {
+      setTotalPages(Number(specificReviseData?.results.totalPages));
+    }
+  }, [specificReviseData]);
+
+  const handlePagination = (e: React.ChangeEvent<unknown>, value: number) => {
+    setCurrentPage(value);
+  };
+  return (
+    <Wrapper>
+      <PageTitleDescription
+        title="수정 요청 목록"
+        description="이전 수정요청에 대해 확인해보세요!"
+      />
+      <StateTab
+        tab1="반영 완료"
+        tab2="미반영"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      {specificReviseData?.results.contributes.length !== 0 ? (
+        <div>
+          {specificReviseData?.results.contributes.map(item => {
+            return (
+              <VoteListCard
+                key={item.contributeId}
+                documentTitle={item.documentTitle}
+                contributeId={item.contributeId}
+                contributeTitle={item.contributeTitle}
+                contributorNickname={item.contributorNickname}
+                createTime={formatDate(item.createdAt)}
+                agreeCount={item.voteSummary.agreeCount}
+                disagreeCount={item.voteSummary.disagreeCount}
+                contributeStatus={item.contributeStatus}
+              />
+            );
+          })}
+          <Center>
+            <Pagination
+              count={totalPages}
+              showFirstButton
+              showLastButton
+              className="my-10 mb-20"
+              page={currentPage}
+              color="primary"
+              onChange={handlePagination}
+            />
+          </Center>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-3 text-white text-center py-10 ">
+          <p className="text-xl font-bold">👽 해당 내용이 없습니다 👽</p>
+          <p>첫 수정요청을 작성해보세요!</p>
+        </div>
+      )}
+    </Wrapper>
+  );
+};
+
+export default Page;
