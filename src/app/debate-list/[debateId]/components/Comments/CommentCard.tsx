998560deb3b937babd479fb61f183a6bbe1cae1f
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-02-18 03:51:13 +0900

    [#255] feat: 토론 댓글 성공실패 알림 구현

--- src/app/debate-list/[debateId]/components/Comments/CommentCard.tsx
+++ src/app/debate-list/[debateId]/components/Comments/CommentCard.tsx
@@ -1,8 +1,14 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import ReportModal from '@/components/Common/ReportModal';
-import { deleteComment, updateComment } from '@/service/debate/comment';
-import { Box, useDisclosure } from '@chakra-ui/react';
+import {
+  DeleteCommentArgs,
+  UpdateCommentArgs,
+  deleteComment,
+  updateComment,
+} from '@/service/debate/comment';
+import { Box, useDisclosure, useToast } from '@chakra-ui/react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import EditCommentForm from './CreateComment/EditCommentForm';
 import CommentDisplay from './CreateComment/CommentDisplay';
 
@@ -34,27 +40,77 @@ const CommentCard: React.FC<DebateCommentProps> = ({
   const [isEdit, setIsEdit] = useState<boolean>(false);
   const [updatedComment, setUpdatedComment] = useState<string>(commentContent);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+  const queryClient = useQueryClient();
 
-  const handleDeleteComment = () => {
-    deleteComment(commentId, debateId);
-    setIsChanged(prev => {
-      return !prev;
-    });
-  };
   const handleEditComment = () => {
     setIsEdit(prev => {
       return !prev;
     });
   };
 
+  const deleteCommentmutation = useMutation<void, Error, DeleteCommentArgs>({
+    mutationFn: deleteComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['comments', commentId] });
+      toast({
+        title: `댓글 삭제 성공!`,
+        description: '댓글 삭제에 성공했습니다!',
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      });
+      setIsChanged(prev => {
+        return !prev;
+      });
+    },
+    onError: error => {
+      console.error('댓글 삭제 실패: ', error);
+      toast({
+        title: '삭제 실패',
+        description: error.message,
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      });
+    },
+  });
+
+  const { mutate: updateCommentMutate } = useMutation<
+    void,
+    Error,
+    UpdateCommentArgs
+  >({
+    mutationFn: updateComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['comments', debateId] });
+      setIsChanged(prev => {return !prev});
+      setIsEdit(prev => {return !prev});
+      toast({
+        title: `댓글 수정 성공!`,
+        description: '댓글 수정이 완료되었습니다.',
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      });
+    },
+    onError: error => {
+      console.error('댓글 수정 실패: ', error);
+      toast({
+        title: '댓글 수정 실패',
+        description: error.message,
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      });
+    },
+  });
+  const handleDeleteComment = () => {
+    deleteCommentmutation.mutate({ commentId, debateId });
+  };
+
   const handleUpdateComment = () => {
-    updateComment(commentId, updatedComment, debateId);
-    setIsChanged(prev => {
-      return !prev;
-    });
-    setIsEdit(prev => {
-      return !prev;
-    });
+    updateCommentMutate({ commentId, newContent: updatedComment, debateId });
   };
 
   return (
@@ -62,7 +118,7 @@ const CommentCard: React.FC<DebateCommentProps> = ({
       <ReportModal
         isOpen={isOpen}
         onClose={onClose}
-        title="comment"
+        type="comment"
         dataId={commentId}
       />
       {isEdit ? (
