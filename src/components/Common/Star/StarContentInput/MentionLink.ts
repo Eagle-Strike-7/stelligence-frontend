80f312b1fb47b5686741b474f7aef83024829204
moonjw0 <jjwon1230@naver.com>
2024-02-17 14:46:24 +0900

    [#207] fix: 언급링크 적용범위 에러 수정

--- src/components/Common/Star/StarContentInput/MentionLink.ts
+++ src/components/Common/Star/StarContentInput/MentionLink.ts
@@ -17,13 +17,14 @@ export const getMentionId = async (selectedText: string) => {
     });
     const { data } = response;
     if (data.results.length === 0) {
-      throw new Error('해당하는 문서가 없습니다.');
       alert('해당하는 문서가 없습니다.');
+      throw new Error('해당하는 문서가 없습니다.');
     } else {
       const starId = data.results[0].documentId;
       return starId;
     }
   } catch (error) {
+    alert('해당하는 문서가 없습니다.');
     throw new Error('해당하는 문서가 없습니다.');
   }
 };
@@ -46,13 +47,23 @@ const LinkifyExtension = Extension.create({
                 // 사용자가 입력을 완료할 때 (두 번째 ']' 입력 후) 패턴 검사
                 const { doc } = view.state; // ProseMirror에서 현재 문서를 나타내는 객체
                 const text = doc.textBetween(0, doc.content.size, '\n'); // 현재 문서의 전체 텍스트를 추출
+
                 const matches = Array.from(text.matchAll(/\[\[(.*?)\]\]/g)); // [[...]] 형식에 매칭되는 모든 패턴을 추출
                 matches.forEach(async match => {
                   const selectedText = match[1];
-                  const start = match.index;
+                  const fullMatchText = match[0];
 
-                  if (start !== undefined) {
-                    const end = start + match[0].length + 2;
+                  if (match.index !== undefined) {
+                    let start = match.index + 1; // 공백 안 밀리게 1추가
+                    const textBeforeMatch = text.slice(0, start);
+                    const newlinesBeforeMatch = (
+                      textBeforeMatch.match(/\n/g) || []
+                    ).length; // \n개수만큼 앞으로 당겨져서 원래 글의 \n의 개수를 세기 (null일때 빈배열 처리)
+
+                    // '\n' 문자의 개수만큼 start 인덱스를 조정
+                    start += newlinesBeforeMatch;
+
+                    const end = start + fullMatchText.length;
                     const documentId = await getMentionId(selectedText);
 
                     if (documentId) {
