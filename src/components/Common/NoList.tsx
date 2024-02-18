5ef657dcf82a22765d59bab6cf4c7b8d79fbfc12
LeeDahee <dduosy9@gmail.com>
2024-02-18 16:25:47 +0900

    [#257] style: 목록 없음 컴포넌트 수정

--- src/components/Common/NoList.tsx
+++ src/components/Common/NoList.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const NoList = () => {
+const NoList = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
   return (
     <div className="flex flex-col gap-3 text-white text-center py-10 ">
-      <p className="text-xl font-bold">목록이 없습니다</p>
+      <p className="text-xl font-bold">{title}</p>
+      <p>{description}</p>
     </div>
   );
 };
