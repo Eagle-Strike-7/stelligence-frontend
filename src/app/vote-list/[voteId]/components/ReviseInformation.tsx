61149896643e4d9a981ca2aec6a942f7c8b8120e
LeeDahee <dduosy9@gmail.com>
2024-02-18 19:49:11 +0900

    [#259] fix: 상위계층태그 제목으로 변경, 클릭 시 해당 글로 이동

--- src/app/vote-list/[voteId]/components/ReviseInformation.tsx
+++ src/app/vote-list/[voteId]/components/ReviseInformation.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import LabelText from '@/components/Common/LabelText';
 import { ReviseDataResponse } from '@/service/vote/voteService';
 import calculateRemainTime from '@/lib/calculateRemainTime';
+import { Tag } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
 const ReviseInformation = ({
   reviseData,
 }: {
   reviseData: ReviseDataResponse;
 }) => {
+  const router = useRouter();
+  const handleClickParentDocument = () => {
+    router.push(`/stars/${reviseData.results.parentDocumentId}`);
+  };
   return (
     <div className="flex flex-col gap-8">
       <div className="grid grid-cols-2 gap-4">
@@ -15,10 +21,22 @@ const ReviseInformation = ({
           label="글 제목"
           text={reviseData?.results.documentTitle || '없음'}
         />
-        <LabelText
-          label="상위 계층 태그"
-          text={reviseData?.results.parentDocumentId || '없음'}
-        />
+        <div className="flex">
+          <h3 className="font-bold text-md w-36 text-white">상위 계층 태그</h3>
+          {reviseData?.results.parentDocumentTitle ? (
+            <Tag
+              className="text-md text-white"
+              _hover={{
+                cursor: 'pointer',
+              }}
+              onClick={handleClickParentDocument}
+            >
+              {reviseData?.results.parentDocumentTitle}
+            </Tag>
+          ) : (
+            <p className="text-md text-white">없음</p>
+          )}
+        </div>
         <LabelText
           label="수정 요청안 제목"
           text={reviseData?.results.contributeTitle || '없음'}
