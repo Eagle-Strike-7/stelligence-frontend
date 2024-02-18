1500e3869138b184156f9945f87f19c73953d82a
LeeDahee <dduosy9@gmail.com>
2024-02-18 16:16:32 +0900

    [#257] refactor: 목록이 없을 때 공통 컴포넌트로 분리

--- src/app/vote-list/page.tsx
+++ src/app/vote-list/page.tsx
@@ -9,6 +9,7 @@ import { Pagination } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import apiClient from '@/service/login/axiosClient';
 import formatDate from '@/lib/formatDate';
+import NoList from '@/components/Common/NoList';
 
 interface ContributeData {
   contributeId: number;
@@ -170,9 +171,7 @@ const Page = () => {
           </Center>
         </div>
       ) : (
-        <div className="flex flex-col gap-3 text-white text-center py-10 ">
-          <p className="text-xl font-bold">목록이 없습니다</p>
-        </div>
+        <NoList />
       )}
     </Wrapper>
   );
---
+++ src/components/Common/NoList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoList = () => {
+  return (
+    <div className="flex flex-col gap-3 text-white text-center py-10 ">
+      <p className="text-xl font-bold">목록이 없습니다</p>
+    </div>
+  );
+};
+
+export default NoList;
