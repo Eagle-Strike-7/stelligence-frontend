250bd4b6faac9b638c4be74255f08f568810257a
moonjw0 <jjwon1230@naver.com>
2024-02-19 18:06:30 +0900

    [#281] feat: 로그인 여부에 따른 페이지 접근 제한

--- src/app/new-star/page.tsx
+++ src/app/new-star/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import React from 'react';
+import useRequireLogin from '@/hooks/common/useRequireLogin';
 import Wrapper from '@/components/Common/Wrapper';
 import NewStarForm from './components/NewStarForm';
 
 // NOTE : 새 글쓰기 페이지
 const Page = () => {
+  useRequireLogin();
   return (
     <Wrapper>
       <NewStarForm />
--- src/app/stars/[starId]/revise/page.tsx
+++ src/app/stars/[starId]/revise/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import useRequireLogin from '@/hooks/common/useRequireLogin';
 import Wrapper from '@/components/Common/Wrapper';
 import ReviseStarForm from './components/ReviseStarForm';
 
 // NOTE : 수정요청 페이지
 const Page = () => {
+  useRequireLogin();
   return (
     <Wrapper>
       <ReviseStarForm />
