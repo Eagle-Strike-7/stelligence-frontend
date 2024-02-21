3f877a1c44d18839a15d7f71339dbb9b452e2bb9
LeeDahee <dduosy9@gmail.com>
2024-02-21 11:46:29 +0900

    [#295] style: 버튼 간격 조절

--- src/components/Header/LeftNav.tsx
+++ src/components/Header/LeftNav.tsx
@@ -4,7 +4,7 @@ import DebateButton from './DebateButton';
 
 const LeftNav = ({ onClose }: { onClose: () => void }) => {
   return (
-    <div className="flex mobile:flex-col desktop:flex-row mobile:ml-4 desktop:ml-20 w-40">
+    <div className="flex gap-2 mobile:flex-col desktop:flex-row mobile:ml-4 desktop:ml-20 w-40">
       <VoteButton onClose={onClose} />
       <DebateButton onClose={onClose} />
     </div>
