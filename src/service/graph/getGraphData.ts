f0e1b1d3c97158014cb2d050433d98295a6935f2
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-03-14 02:17:39 +0900

    [#337] refactor: 그래프 데이터 fetch 함수 적용

--- src/service/graph/getGraphData.ts
+++ src/service/graph/getGraphData.ts
@@ -3,13 +3,11 @@ import {
   transformLinks,
 } from '@/hooks/graph/transformGraphInfo';
 import { Graph } from '@/types/graph/GraphProps';
-import apiClient from '../login/axiosClient';
 
 const getGraphData = async (): Promise<Graph> => {
-  const response = await apiClient.get(`/api/documents`, {
-    params: { depth: 3 },
-  });
-  const { data } = response;
+  const response = await fetch(`https://api.stelligence.site/api/documents`, { cache: 'no-cache' });
+  const data = await response.json();
+
   const transformedResults = transformResults(data.results.documentNodes);
   const transformedLinks = transformLinks(data.results.links);
 
