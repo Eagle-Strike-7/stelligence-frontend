a9bd5532ad2467d74cd75282890826d22b2fa2d9
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-03-18 23:12:12 +0900

    [#330] feat: 은하수 여행자 아이콘 외부 영역 클릭 시 창 닫히게 구현

--- src/app/info/components/InfoButton.tsx
+++ src/app/info/components/InfoButton.tsx
@@ -1,12 +1,19 @@
-"use client"
+'use client';
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import useOutsideClick from '@/hooks/common/useOutsideClick';
 import InfoMenu from './InfoMenu';
 
 const InfoButton = () => {
+  const infoButtonRef = useRef<HTMLDivElement>(null);
   const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
+
+  useOutsideClick(infoButtonRef, () => {
+    setIsMenuOpened(false);
+  });
+
   return (
-    <div>
+    <div ref={infoButtonRef}>
       <button
         className="fixed bottom-4 right-4 p-2 z-100 w-10 h-10 bg-primary-dark-700 text-white rounded-full hover:bg-primary-dark-800 focus:outline-none focus:ring-2 focus:bg-primary-dark-900 focus:bg-primary-dark-500-opacity-80"
         onClick={() => {
@@ -20,7 +27,7 @@ const InfoButton = () => {
       </button>
 
       {isMenuOpened && (
-        <div className="z-100 ">
+        <div className="z-100">
           <InfoMenu />
         </div>
       )}
