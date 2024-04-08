bedc4adf9e5a83e945644648cc039cfb5a308f90
Nari Jeong <92130993+naringst@users.noreply.github.com>
2024-04-08 15:05:14 +0900

    refactor: 서버 노드 타입 이름 수정

--- src/service/search/getSearchResult.ts
+++ src/service/search/getSearchResult.ts
@@ -1,10 +1,10 @@
-import { SearchResult } from '@/types/graph/GraphProps';
+import { ServerNode } from '@/types/graph/GraphProps';
 import apiClient from '../login/axiosClient';
 
-const getSearchResult = async (input: string): Promise<SearchResult[]> => {
+const getSearchResult = async (input: string): Promise<ServerNode[]> => {
   if (!input.trim()) return [];
   try {
-    const response = await apiClient.get<{ results: SearchResult[] }>(
+    const response = await apiClient.get<{ results: ServerNode[] }>(
       `/api/documents/search`,
       {
         params: { title: input },
@@ -12,8 +12,7 @@ const getSearchResult = async (input: string): Promise<SearchResult[]> => {
     );
     return response.data.results;
   } catch (error) {
-    console.error('Error fetching search results: ', error);
-    throw new Error('Failed to fetch search results');
+    throw new Error('검색 실패');
   }
 };
 
